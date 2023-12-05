3ea08aaf3f30a3d61ae2625ef5bf54d11cf30188
BirdeeHub <robindbor@gmail.com>
2023-12-04 23:52:24 -0800

    NVIDIA

--- configuration.nix
+++ configuration.nix
@@ -156,43 +156,56 @@ in {
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
- #  # Enable OpenGL
+
+  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    nvidiaBusId = "PCI:01:00:0";   # Found with lspci | grep VGA
+    intelBusId = "PCI:00:02:0";   # Found with lspci | grep VGA
+  };
+
+
+  # Enable OpenGL
   # hardware.opengl = {
   #   enable = true;
   #   driSupport = true;
   #   driSupport32Bit = true;
   # };
-#
+# boot.blacklistedKernelModules = ["nouveau"];
 #  # Load nvidia driver for Xorg and Wayland
-#  services.xserver.videoDrivers = ["nvidia"];
-#
-#  hardware.nvidia = {
-#
-#    # Modesetting is required.
-#    modesetting.enable = true;
-#
-#    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-#    powerManagement.enable = false;
-#    # Fine-grained power management. Turns off GPU when not in use.
-#    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-#    powerManagement.finegrained = false;
-#
-#    # Use the NVidia open source kernel module (not to be confused with the
-#    # independent third-party "nouveau" open source driver).
-#    # Support is limited to the Turing and later architectures. Full list of 
-#    # supported GPUs is at: 
-#    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-#    # Only available from driver 515.43.04+
-#    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-#    open = false;
-#
-#    # Enable the Nvidia settings menu,
-# # accessible via `nvidia-settings`.
-#    nvidiaSettings = true;
-#
-#    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-#    package = config.boot.kernelPackages.nvidiaPackages.stable;
-#  };
+# services.xserver.videoDrivers = ["nvidia"];
+
+# hardware.nvidia = {
+
+  # Modesetting is required.
+  # modesetting.enable = true;
+
+  # prime.sync.enable = true;
+
+  # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  # powerManagement.enable = false;
+  # Fine-grained power management. Turns off GPU when not in use.
+  # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  # powerManagement.finegrained = false;
+
+  # Use the NVidia open source kernel module (not to be confused with the
+  # independent third-party "nouveau" open source driver).
+  # Support is limited to the Turing and later architectures. Full list of 
+  # supported GPUs is at: 
+  # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  # Only available from driver 515.43.04+
+  # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  # open = false;
+
+  # Enable the Nvidia settings menu,
+  # accessible via `nvidia-settings`.
+   # nvidiaSettings = true;
+
+   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+   # package = config.boot.kernelPackages.nvidiaPackages.stable;
+ # };
+
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
@@ -231,7 +244,6 @@ in {
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     galculator
     qalculate-qt
-    # alacritty
     alakitty
     pciutils
     glxinfo
