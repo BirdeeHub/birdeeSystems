dbba6dea27f9a402697bf20ebd2edfde5c19599e
BirdeeHub <robindbor@gmail.com>
2024-01-08 23:12:39 -0800

    updates

--- configuration.nix
+++ configuration.nix
@@ -138,10 +138,11 @@ in {
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gnome3";
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
