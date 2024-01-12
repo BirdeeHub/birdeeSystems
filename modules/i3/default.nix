d0fa5f1315660763d80565e1739b81e44fc9cdda
BirdeeHub <robindbor@gmail.com>
2024-01-12 06:16:23 -0800

    updates

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -35,20 +35,19 @@
     environment.shellAliases = {
       leftMon = ''${pkgs.writeScript "leftMonFlexible.sh" (/*bash*/''
         #!/usr/bin/env bash
-        rate=59.95; mode="1920x1080" side="l"
-        [[ $# > 0 ]] && rate=$1
-        [[ $# > 1 ]] && mode=$2
-        [[ $# > 2 ]] && side=$3
+        rate=59.95; mode="1920x1080"; side="l";
+        current="HDMI-1"; primary="eDP-1";
+        [[ $# > 0 ]] && rate=$1 && shift 1
+        [[ $# > 0 ]] && mode=$1 && shift 1
+        [[ $# > 0 ]] && side=$1 && shift 1
+        [[ $# > 0 ]] && current=$1 && shift 1
+        [[ $# > 0 ]] && primary=$1 && shift 1
         if [[ $side == "l" ]]; then
           side="--left-of"
         elif [[ $side == "r" ]]; then
           side="--right-of"
-        else
-          side=$3
         fi
-        rest=()
-        [[ $# > 3 ]] && rest=''${@:4:$(($#-3))}
-        ${xrandr} --output HDMI-1 $side eDP-1 --rate $rate --mode $mode ''${rest[@]}
+        ${xrandr} --output $current $side $primary --rate $rate --mode $mode $@
       '')}'';
       leftMonPrf = /*bash*/ "${xrandr} --output HDMI-1 --left-of eDP-1 --preferred";
     };
@@ -57,8 +56,9 @@
     # it doesnt even have to be this big script, even just xrandr --auto...
     services.udev = {
       enable = true;
+        # ACTION=="change", SUBSYSTEM=="drm", ENV{HOTPLUG}=="1", RUN+="${randrMemory}"
       extraRules = ''
-        ACTION=="change", SUBSYSTEM=="drm", ENV{HOTPLUG}=="1", RUN+="${randrMemory}"
+        ACTION=="change", KERNEL=="card0", SUBSYSTEM=="drm",  RUN+="${pkgs.xorg.xrandr}/bin/xrandr --auto"
       '';
     };
 
