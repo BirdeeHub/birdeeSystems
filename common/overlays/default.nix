4b623d4cf0d2af8fe4e0be2957d150123e0c8604
BirdeeHub <robindbor@gmail.com>
2024-01-14 06:38:22 -0800

    OMP Pinned

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -1,7 +1,5 @@
 /*
 This file imports overlays defined in the following format.
-Plugins will still only be downloaded if included in a category.
-You may copy paste this example into a new file and then import that file here.
 */
 # Example overlay:
 /*
@@ -22,7 +20,6 @@ overlay
     # the name here will be the name used when importing items from it in your flake.
     # i.e. these items will be accessed as pkgs.nixCatsBuilds.thenameofthepackage
 
-    #except for this one which just outputs to the base pkgs set
     pinnedVersions = import ./pinnedVersions.nix;
 
   };
