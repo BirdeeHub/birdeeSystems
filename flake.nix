bedb5173d5a81e4208b24b98e7bc1c291996fa2e
BirdeeHub <robindbor@gmail.com>
2024-01-17 13:41:53 -0800

    omp hit nixpkgs-unstable (but not nixos-unstable)

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs_4_OMP.url = "github:nixos/nixpkgs/757637581797f148c50977b6127147c5d298f9e9";
+    nixpkgs_4_OMP.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
