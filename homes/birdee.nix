0c40e822fcfcd69c9c4d4cd3060e60091992cefd
BirdeeHub <robindbor@gmail.com>
2024-01-17 14:07:36 -0800

    editor variable

--- homes/birdee.nix
+++ homes/birdee.nix
@@ -40,7 +40,7 @@ in {
     l  = "ls -alh";
   };
   home.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR = "birdeeVim";
     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
   };
 
