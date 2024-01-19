b35afeda666c4f5c775f74ba3aeeb11ad0d75641
BirdeeHub <robindbor@gmail.com>
2024-01-19 14:08:16 -0800

    mac updates

--- systems/dustbook/mon/bootUpMonitorScript.sh
+++ systems/dustbook/mon/bootUpMonitorScript.sh
@@ -1,4 +1,4 @@
-$xrandr --output LVDS-1 --primary --preferred
+$xrandr --output LVDS-2 --primary --preferred
 # $xrandr --newmode "1920x1080_custom" 130.18 1920 2008 2052 2200 1080 1084 1089 1125 +hsync +vsync
 # $xrandr --addmode HDMI-1-1 "1920x1080_custom"
 # $xrandr --output HDMI-1-1 --left-of eDP-1-1 --mode "1920x1080_custom"
