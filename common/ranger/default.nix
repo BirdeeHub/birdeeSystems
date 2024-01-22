4190405e46f7743d5659ab458d3a35b7a8fb0d29
BirdeeHub <robindbor@gmail.com>
2024-01-22 01:50:05 -0800

    better mapping

--- common/ranger/default.nix
+++ common/ranger/default.nix
@@ -6,13 +6,12 @@
   };
     # the files in this directory will not be sourced, they are there as an example
     # they are all the default values.
-    # one day, I would like to source them via a command here.
-    # for now, I am only just learning how to use ranger for the first time.
+    # I am only just learning how to use ranger for the first time.
   config = lib.mkIf config.birdeeMods.ranger.enable (let
     cfg = config.birdeeMods.ranger;
     ranger = pkgs.writeShellScriptBin "ranger" (let
     in ''
-      ${pkgs.ranger}/bin/ranger --cmd="map <C-d> shell ${pkgs.xdragon}/bin/dragon -a -x %p" "$@"
+      ${pkgs.ranger}/bin/ranger --cmd="map <C-Y> shell ${pkgs.xdragon}/bin/dragon -a -x %p" "$@"
     '');
   in {
     home.packages = with pkgs; [
