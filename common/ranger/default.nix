afb55867f6d2a262bfbc622a34f9390527ff97eb
BirdeeHub <robindbor@gmail.com>
2024-01-22 17:12:58 -0800

    ranger refactor

--- common/ranger/default.nix
+++ common/ranger/default.nix
@@ -4,22 +4,19 @@
       enable = lib.mkEnableOption "birdee's ranger";
     };
   };
-    # the files in this directory will not be sourced, they are there as an example
-    # they are all the default values.
-    # I am only just learning how to use ranger for the first time.
   config = lib.mkIf config.birdeeMods.ranger.enable (let
     cfg = config.birdeeMods.ranger;
-    ranger_commands = [
-      ''map <C-Y> shell ${pkgs.xdragon}/bin/dragon -a -x %p''
-      ''map y<C-Y> shell ${pkgs.xdragon}/bin/dragon --all-compact -x %p''
-      ''set mouse_enabled!''
-      ''map ps shell echo "$(xclip -o) ." | xargs cp -r''
-    ];
-    reformattedCommands = builtins.map (item: "--cmd='${item}'") ranger_commands;
-    cattedCommands = builtins.concatStringsSep " " reformattedCommands;
+    ranger_commands = pkgs.writeText "nixRangerRC.conf" (let
+      dragon = ''${pkgs.xdragon}/bin/dragon'';
+      in ''
+        map <C-Y> shell ${dragon} -a -x %p
+        map y<C-Y> shell ${dragon} --all-compact -x %p
+        set mouse_enabled!
+        map ps shell echo "$(xclip -o) ." | xargs cp -r
+      '');
     ranger = pkgs.writeShellScriptBin "ranger" (let
     in ''
-      ${pkgs.ranger}/bin/ranger ${cattedCommands} "$@"
+      ${pkgs.ranger}/bin/ranger --cmd='source ${ranger_commands}' "$@"
     '');
   in {
     home.packages = with pkgs; [
