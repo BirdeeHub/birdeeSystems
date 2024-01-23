158299a2a4255d5e26fe35ab6ac3a9c34f85e557
BirdeeHub <robindbor@gmail.com>
2024-01-22 22:15:23 -0800

    typo fix

--- systems/PCs/dustbook/default.nix
+++ systems/PCs/dustbook/default.nix
@@ -11,7 +11,7 @@ in {
 
   birdeeMods = {
     i3.bootUpMonScript = ./mon/bootUpMonitorScript.sh;
-    lightdm.SessionCommands = ''
+    lightdm.sessionCommands = ''
       ${pkgs.xorg.xrdb}/bin/xrdb -merge <${pkgs.writeText "Xresources" ''
         Xft.dpi: 80
       ''}
