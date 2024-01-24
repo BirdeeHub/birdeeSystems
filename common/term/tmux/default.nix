5b51636494381a9bad0da14f742673b396c338a9
BirdeeHub <robindbor@gmail.com>
2024-01-24 03:58:44 -0800

    finally... a good script to launch it

--- common/term/tmux/default.nix
+++ common/term/tmux/default.nix
@@ -11,14 +11,14 @@
   };
   config = lib.mkIf config.birdeeMods.tmux.enable (let
     tmuxLaunchScript = pkgs.writeShellScript "tmuxAlias1" (/*bash*/''
-      unattached_tmux_sessions=$(tmux list-sessions | grep -v "(attached)")
-      number_of_unattached=$(echo "$unattached_tmux_sessions" | wc -l)
-      if [ "$number_of_unattached" != "0" ]; then
+      unattached_tmux_sessions=$(tmux list-sessions -F '#{session_name}: #{?session_attached,attached,not attached}' | awk '$NF == "not attached" {print $1}')
+      number_of_unattached=$(tmux list-sessions -F '#{?session_attached,1,0}' | grep -c '0')
+      if [[ $number_of_unattached -ne 0 ]]; then
         selected_session=$(echo "$unattached_tmux_sessions" | head -n 1)
         selected_session_name=''${selected_session%: [0-9]*}
         tmux new-session -At $selected_session_name
       else
-        tmux new-session -At $(tmux list-sessions | wc -l)
+        tmux new-session
       fi
     '');
   in {
