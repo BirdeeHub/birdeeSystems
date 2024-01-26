89c5d29f96ee24a7879bb09f1382594b527f2e1f
BirdeeHub <robindbor@gmail.com>
2024-01-25 19:38:44 -0800

    apparently forgot to xdg.enable

--- common/i3MonMemory/defaults/Xothers.sh
+++ common/i3MonMemory/defaults/Xothers.sh
@@ -3,3 +3,6 @@
 if [[ $1 == "HDMI-1" ]]; then
     xrandr --output HDMI-1 --left-of eDP-1 --preferred
 fi
+if [[ $1 == "DP-1" ]]; then
+    xrandr --output DP-1 --left-of LVDS-2
+fi
--- homes/birdee.nix
+++ homes/birdee.nix
@@ -65,6 +65,7 @@ in {
     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
   };
 
+  xdg.enable = true;
   xdg.userDirs = {
     enable = true;
     desktop = "${config.home.homeDirectory}/Desktop";
