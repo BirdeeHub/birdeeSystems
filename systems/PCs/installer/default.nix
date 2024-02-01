2635439c4efae9d20ff255c30d595bd931680251
BirdeeHub <robindbor@gmail.com>
2024-02-01 00:27:29 -0800

    alias

--- systems/PCs/installer/default.nix
+++ systems/PCs/installer/default.nix
@@ -12,6 +12,8 @@
 
   environment.shellAliases = {
     install-birdeeOS = "${pkgs.writeShellScript "install-birdeeOS" ''
+      sudo nix run github:nix-community/disko -- --mode disko /iso/tmp/birdeeSystems/systems/PCs/disko/$1.nix
+      sudo nixos-install --flake /iso/tmp/birdeeSystems#$2
     ''}";
   };
 
