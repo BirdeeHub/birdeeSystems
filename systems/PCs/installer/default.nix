994f500091bd43bc2ca7185bcb28ab8bd932ac18
BirdeeHub <robindbor@gmail.com>
2024-02-01 13:32:12 -0800

    added some more stuff to installer

--- systems/PCs/installer/default.nix
+++ systems/PCs/installer/default.nix
@@ -2,12 +2,21 @@
   imports = with system-modules; [
     birdeeVim.module
     "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+    term.alacritty
+    shell.bash
+    shell.zsh
+    shell.fish
   ];
 
   boot.kernelModules = [ "kvm-intel" "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
 
   # disko.enableConfig = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  # Allow flakes and new command
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.shellAliases = {
     install-birdeeOS = "${pkgs.writeShellScript "install-birdeeOS" ''
@@ -21,6 +30,15 @@
   ];
 
   isoImage.isoBaseName = "birdeeSystems_Installer";
+
+  birdeeMods = {
+    zsh.enable = true;
+    bash.enable = true;
+    fish.enable = true;
+    alacritty.enable = true;
+  };
+
+  users.defaultUserShell = pkgs.zsh;
 
   fonts.packages = with pkgs; [
     openmoji-color
@@ -43,10 +61,6 @@
     packageNames = [ "noAInvim" ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  # Allow flakes and new command
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   services.xserver.libinput.enable = true;
   services.xserver.libinput.touchpad.disableWhileTyping = true;
   environment.systemPackages = (let
@@ -91,6 +105,10 @@
     ranger
     git
     xsel
+    ntfs3g
+    findutils
+    _7zz
+    lshw
     xclip
   ]);
 
