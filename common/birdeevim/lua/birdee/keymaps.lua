878ae1589e78784764889f14127dd8ffb44a3119
BirdeeHub <robindbor@gmail.com>
2024-02-13 15:07:06 -0800

    added keybind and updated stuff

--- common/birdeevim/lua/birdee/keymaps.lua
+++ common/birdeevim/lua/birdee/keymaps.lua
@@ -33,6 +33,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('n', '<leader>t', [[:terminal]], { desc = 'open terminal in current buffer' })
+
 
 -- these 3 jankily fix which-key related errors for some reason
 vim.keymap.set('n', '<C-W>', '<c-w>', { desc = '+window'})
