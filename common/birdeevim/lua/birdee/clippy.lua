7d23240a79d73380cd974f8b12ce5d06e5f7f494
BirdeeHub <robindbor@gmail.com>
2024-02-20 16:54:03 -0800

    updates

--- common/birdeevim/lua/birdee/clippy.lua
+++ common/birdeevim/lua/birdee/clippy.lua
@@ -16,7 +16,9 @@ vim.keymap.set({"n", "v", "x"}, '<leader>Y', '"+yy', { noremap = true, silent = 
 vim.keymap.set({"n", "v", "x"}, '<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
 vim.keymap.set({"n", "v", "x"}, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
 vim.keymap.set('i', '<C-p>', '<C-r><C-p>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
-vim.keymap.set("x", "<leader>P", '"_dP', { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
+
+-- this is now included in visual mode by default as P keybind
+-- vim.keymap.set("x", "<leader>P", '"_dP', { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
 
 
 -- so, my normal mode <leader>y randomly didnt accept motions. 
