9960d0d0be297428c09d19e29222714749379369
rdeboer-pzOtJY <robindbor@gmail.com>
2024-02-28 17:25:48 -0800

    updates

--- common/term/alacritty/default.nix
+++ common/term/alacritty/default.nix
@@ -1,5 +1,32 @@
 isHomeModule:
 {config, pkgs, self, inputs, lib, ... }@args: let
+
+  alakitty-toml = isHomeModule: { config, pkgs, inputs, self, lib, ... }: let
+  in (/*toml*/''
+    # https://alacritty.org/config-alacritty.html
+    # [env]
+    # TERM = "xterm-256color"
+
+    [font]
+    size = 11.0
+
+    [font.bold]
+    family = "FiraMono Nerd Font"
+    style = "Bold"
+
+    [font.bold_italic]
+    family = "FiraMono Nerd Font"
+    style = "Bold Italic"
+
+    [font.italic]
+    family = "FiraMono Nerd Font"
+    style = "Italic"
+
+    [font.normal]
+    family = "FiraMono Nerd Font"
+    style = "Regular"
+  '');
+
 in {
   options = {
     birdeeMods.alacritty = with lib.types; {
@@ -12,13 +39,13 @@ in {
   };
   config = lib.mkIf config.birdeeMods.alacritty.enable (let
     cfg = config.birdeeMods.alacritty;
-    alacrittyToml = pkgs.writeText "alacritty.toml" (builtins.concatStringsSep "\n" [
-      (import ./alacritty.nix isHomeModule args)
+    final-alakitty-toml = pkgs.writeText "alacritty.toml" (builtins.concatStringsSep "\n" [
+      (alakitty-toml isHomeModule args)
       cfg.extraToml
       ]);
     alakitty = pkgs.writeScriptBin "alacritty" ''
       #!/bin/sh
-      exec ${pkgs.alacritty}/bin/alacritty --config-file ${alacrittyToml} "$@"
+      exec ${pkgs.alacritty}/bin/alacritty --config-file ${final-alakitty-toml} "$@"
     '';
   in (if isHomeModule then {
     home.packages = [ alakitty ];
