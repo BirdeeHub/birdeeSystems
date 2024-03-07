2adcd333e848ff15328bb5f336950a8673e170a5
rdeboer-pzOtJY <robindbor@gmail.com>
2024-03-07 15:23:27 -0800

    updates

--- common/birdeevim/lua/birdee/plugins/init.lua
+++ common/birdeevim/lua/birdee/plugins/init.lua
@@ -80,20 +80,65 @@ require('Comment').setup()
   -- require('fidget').setup()
 require('lualine').setup({
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = colorschemer,
-    component_separators = '|',
-    section_separators = '',
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
   },
   sections = {
+    lualine_a = {
+      'mode',
+    },
+    lualine_b = {
+      'branch',
+      'diff',
+      'diagnostics',
+      'buffers',
+    },
     lualine_c = {
       {
         'filename', path = 1, status = true,
       },
-      'buffers',
       -- 'lsp_progress',
     },
+    lualine_x = {
+      'encoding',
+      'fileformat',
+      'filetype',
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
   },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {
+      'buffers',
+    },
+    lualine_c = {
+      {
+        'filename', path = 1, status = true,
+      },
+    },
+    lualine_x = {'filetype'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 })
 require('fidget').setup({})
 require('nvim-surround').setup()
