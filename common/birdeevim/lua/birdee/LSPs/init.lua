c9a02ea19c634d366ba1ed6fd9df1749441deb0d
rdeboer-pzOtJY <robindbor@gmail.com>
2024-03-09 04:47:44 -0800

    updates

--- common/birdeevim/lua/birdee/LSPs/init.lua
+++ common/birdeevim/lua/birdee/LSPs/init.lua
@@ -33,7 +33,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --     buffer = event.buf,
     --     callback = vim.lsp.buf.document_highlight,
     --   })
-    --
+
     --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
     --     buffer = event.buf,
     --     callback = vim.lsp.buf.clear_references,
