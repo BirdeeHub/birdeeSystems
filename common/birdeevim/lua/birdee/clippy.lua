82beaf54a4b5a86a25eea1aebe2bc3df8d141864
rdeboer-pzOtJY <robindbor@gmail.com>
2024-03-13 02:06:22 -0700

    changed some keybinds

--- common/birdeevim/lua/birdee/clippy.lua
+++ common/birdeevim/lua/birdee/clippy.lua
@@ -13,7 +13,7 @@ vim.keymap.set("n", '<leader>y', '"+y', { noremap = true, silent = true, desc = 
 vim.keymap.set({"v", "x"}, '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
 vim.keymap.set({"n", "v", "x"}, '<leader>yy', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({"n", "v", "x"}, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
-vim.keymap.set({"n", "v", "x"}, '<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
+vim.keymap.set({"n"}, 'v<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
 vim.keymap.set({"n", "v", "x"}, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
 vim.keymap.set('i', '<C-p>', '<C-r><C-p>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
 
