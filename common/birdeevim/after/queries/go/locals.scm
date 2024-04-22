3c1584ee4cfe93357435b51a4350d007aa2202c1
BirdeeHub <robindbor@gmail.com>
2024-04-22 03:45:41 -0700

    updates

--- common/birdeevim/after/queries/go/locals.scm
+++ common/birdeevim/after/queries/go/locals.scm
@@ -5,10 +5,6 @@
 (field_declaration
   name: (field_identifier) @local.definition.field)
 
-(method_elem
-  name: (field_identifier) @function.method.name
-  parameters: (parameter_list) @function.method.parameter_list) @local.interface.method.declaration
-
 (type_declaration
   (type_spec
     name: (type_identifier) @local.name
