60138284909d18d8b20ef320e6cd1d415d83b721
BirdeeHub <robindbor@gmail.com>
2024-06-24 17:49:43 -0700

    updates

--- flake.nix
+++ flake.nix
@@ -34,9 +34,7 @@
     templ.url = "github:a-h/templ";
     neorg-overlay.url = "github:nvim-neorg/nixpkgs-neorg-overlay";
     lz-n = {
-      url = "github:BirdeeHub/lz.n";
-      # url = "github:nvim-neorocks/lz.n";
-      # url = "git+file:/home/birdee/Projects/lz.n";
+      url = "github:nvim-neorocks/lz.n";
       inputs.nixpkgs.follows = "nixpkgsNV";
     };
     "plugins-render-markdown" = {
