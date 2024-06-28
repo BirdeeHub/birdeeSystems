2d8f54e1e8e274a5606bcc4ad733123b5e5b2863
BirdeeHub <robindbor@gmail.com>
2024-06-28 02:05:53 -0700

    updates

--- common/birdeevim/lua/birdee/debug_handler.lua
+++ common/birdeevim/lua/birdee/debug_handler.lua
@@ -4,24 +4,24 @@ local states = {}
 local M = {
   ---@type lz.n.Handler
   handler = {
-	-- this field does nothing but it does stop others from using is_loaded,
-	-- which is good because we overwrite the value in getAllPlugins
-	spec_field = "is_loaded",
-	---@param plugin lz.n.Plugin
-	del = function (plugin)
-	  states[plugin.name] = plugin.name
-	end,
-	add = function(_) end,
+    -- this field does nothing but it does stop others from using is_loaded,
+    -- which is good because we overwrite the value in getAllPlugins
+    spec_field = "is_loaded",
+    ---@param plugin lz.n.Plugin
+    del = function (plugin)
+      states[plugin.name] = plugin.name
+    end,
+    add = function(_) end,
   },
 }
 
 function M.get_all_plugins()
   local result = vim.deepcopy(require("lz.n.state").plugins)
   for _, name in pairs(states) do
-	if result[name] ~= nil then
-	  ---@diagnostic disable-next-line: inject-field
-	  result[name].is_loaded = true
-	end
+    if result[name] ~= nil then
+      ---@diagnostic disable-next-line: inject-field
+      result[name].is_loaded = true
+    end
   end
   return result
 end
