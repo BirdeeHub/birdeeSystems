ce2df09f740a49f0ab17197dd3537f2b2c194014
BirdeeHub <robindbor@gmail.com>
2024-07-13 08:41:40 -0700

    prep for refactor

--- common/birdeevim/pack/personalplugins/opt/color_picker/lua/color_picker/init.lua
+++ common/birdeevim/pack/personalplugins/opt/color_picker/lua/color_picker/init.lua
@@ -1,13 +1,16 @@
--- NOTE: in this order
--- TODO: add history feature (should store as hex so that it can be easily generalized for other color spaces)
--- TODO: add ability to customize keybinds
+-- NOTE: in roughly this order
 -- TODO: refactor such that 2 pickers can be provided and a gradient view is generated for it.
+-- TODO: add history feature (should store as hex so that it can be easily generalized for other color spaces)
+-- TODO: add ability to export to vim.fn.setreg('+', color) instead of buffer
+-- TODO: add ability to customize keybinds
 -- TODO: add cmyk, hsl, and lab color type pickers
 local picker = {};
+local color = require("color_picker.color").get()
+local hsv = require("color_picker.hsv").get()
 
-picker.colorPicker = function() require("color_picker.color").get():init() end
-picker.gradientPicker = function() require("color_picker.gradient").get():init() end
+picker.colorPicker = function() color:init() end
+picker.gradientPicker = function() require("color_picker.gradient"):init() end
 picker.hsvGradientPicker = function() require("color_picker.hsvgradient"):init() end
-picker.huePicker = function() require("color_picker.huesatv"):init() end
+picker.huePicker = function() hsv:init() end
 
 return picker;
