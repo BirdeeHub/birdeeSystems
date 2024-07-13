3752d3a1e251ac71a306643a4eca297a02796486
BirdeeHub <robindbor@gmail.com>
2024-07-13 11:30:00 -0700

    refactoring color picker

--- common/birdeevim/pack/personalplugins/opt/color_picker/lua/color_picker/utils.lua
+++ common/birdeevim/pack/personalplugins/opt/color_picker/lua/color_picker/utils.lua
@@ -45,20 +45,6 @@ utils.toStr = function (color)
 end
 ---_
 
----+ Icon: " " Title: "rgb number to table converter" BorderL: " " BorderR: " "
---- @param color number Number returned by "nvim_get_hl()"
---- @return color_rgb # Table with r, g, b values
-utils.rgbToRgb = function (color)
-	local hex = string.format("%x", color);
-
-	return {
-		r = tonumber(string.sub(hex, 1, 2), 16),
-		g = tonumber(string.sub(hex, 3, 4), 16),
-		b = tonumber(string.sub(hex, 5, 6), 16),
-	};
-end
---_
-
 ---+ Icon: " " Title: "hex color to table converter" BorderL: " " BorderR: " "
 --- @param color string Hexadecimal color code
 --- @return color_rgb # Table with r, g, b values
@@ -78,59 +64,6 @@ utils.hexToRgb = function (color)
 			b = tonumber(string.sub(hex, 5, 6), 16),
 		};
 	end
-end
---_
-
----+ Icon: " " Title: "Do eased interpolation" BorderL: " " BorderR: " "
---- @param ease string The name of the easing function to use
---- @param from number Starting value
---- @param to number Final value
---- @param position number % position from the start value to the final value
---- @return number
-utils.ease = function(ease, from, to, position)
-	local easeValue = 0;
-
-	if ease == "linear" then
-		easeValue = position;
-	elseif ease == "ease-in-sine" then
-		easeValue = 1 - math.cos((position * math.pi) / 2);
-	elseif ease == "ease-out-sine" then
-		easeValue = math.sin((position * math.pi) / 2);
-	elseif ease == "ease-in-out-sine" then
-		easeValue = -(math.cos(position * math.pi) - 1) / 2;
-	elseif ease == "ease-in-quad" then
-		easeValue = position ^ 2;
-	elseif ease == "ease-out-quad" then
-		easeValue = 1 - ((1 - position) ^ 2);
-	elseif ease == "ease-in-out-quad" then
-		easeValue = position < 0.5 and 2 * (position ^ 2) or 1 - (((-2 * position + 2) ^ 2) / 2);
-	elseif ease == "ease-in-cubic" then
-		easeValue = position ^ 3;
-	elseif ease == "ease-out-cubic" then
-		easeValue = 1 - ((1 - position) ^ 3);
-	elseif ease == "ease-in-out-cubic" then
-		easeValue = position < 0.5 and 4 * (position ^ 3) or 1 - (((-2 * position + 2) ^ 3) / 2);
-	elseif ease == "ease-in-quart" then
-		easeValue = position ^ 4;
-	elseif ease == "ease-out-quart" then
-		easeValue = 1 - ((1 - position) ^ 4);
-	elseif ease == "ease-in-out-quart" then
-		easeValue = position < 0.5 and 8 * (position ^ 4) or 1 - (((-2 * position + 2) ^ 4) / 2);
-	elseif ease == "ease-in-quint" then
-		easeValue = position ^ 5;
-	elseif ease == "ease-out-quint" then
-		easeValue = 1 - ((1 - position) ^ 5);
-	elseif ease == "ease-in-out-quint" then
-		easeValue = position < 0.5 and 16 * (position ^ 5) or 1 - (((-2 * position + 2) ^ 5) / 2);
-	elseif ease == "ease-in-circ" then
-		easeValue = 1 - math.sqrt(1 - (position ^ 2));
-	elseif ease == "ease-out-circ" then
-		easeValue = math.sqrt(1 - ((position - 1) ^ 2));
-	elseif ease == "ease-in-out-circ" then
-		easeValue = position < 0.5 and (1 - math.sqrt(1 - ((2 * y) ^ 2))) / 2 or (math.sqrt(1 - ((-2 * y + 2) ^ 2)) + 1) / 2;
-	end
-
-	return from + ((to - from) * easeValue);
 end
 --_
 
