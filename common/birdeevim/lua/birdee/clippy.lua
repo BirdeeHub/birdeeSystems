069504ba3ac90f848d71ee77b4f71fe2b360fc85
BirdeeHub <robindbor@gmail.com>
2024-07-16 04:05:36 -0700

    updates

--- common/birdeevim/lua/birdee/clippy.lua
+++ common/birdeevim/lua/birdee/clippy.lua
@@ -9,8 +9,6 @@
 -- So, meet clippy.lua
 
 vim.keymap.set({ "n", "v", "x" }, '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
-vim.keymap.set({ "n", "v", "x" }, '<leader>yy', '"+yy',
-  { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({ "n", "v", "x" }, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({ "n" }, 'v<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
 vim.keymap.set({ "n", "v", "x" }, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
