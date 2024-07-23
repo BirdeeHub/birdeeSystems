8630fe86d01fe70e3cc4e9f5eec795c81e28a840
BirdeeHub <robindbor@gmail.com>
2024-07-22 23:50:46 -0700

    updates

--- common/birdeevim/lua/birdee/opts.lua
+++ common/birdeevim/lua/birdee/opts.lua
@@ -26,7 +26,7 @@ vim.o.smarttab = true
 -- vim.o.smartindent = true
 -- vim.o.indentexpr = true
 vim.o.autoindent = true
-vim.o.cpoptions = (vim.o.cpoptions or '') .. 'I'
+vim.opt.cpoptions:append('I')
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
