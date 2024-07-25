b0f70ed65f36d52aa55057cc8474d9aaa81bd829
BirdeeHub <robindbor@gmail.com>
2024-07-25 06:47:12 -0700

    updates

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/acb4f0e9bfa8ca2d6fca5e692307b5c994e7dbda";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    minesweeper.url = "github:BirdeeHub/minesweeper/nix";
+    minesweeper.url = "github:BirdeeHub/MySweep";
     minesweeper.inputs.nixpkgs.follows = "nixpkgs";
     minesweeper.inputs.flake-utils.follows = "flake-utils";
     minesweeper.inputs.nix-appimage.follows = "nix-appimage";
