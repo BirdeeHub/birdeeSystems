4e9dd4fbbb57bb85489cd696883710fad844923a
BirdeeHub <robindbor@gmail.com>
2024-07-26 11:41:15 -0700

    updates

--- flake.nix
+++ flake.nix
@@ -22,8 +22,6 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
     minesweeper.url = "github:BirdeeHub/minesweeper";
     minesweeper.inputs.nixpkgs.follows = "nixpkgsNV";
-    minesweeper.inputs.flake-utils.follows = "flake-utils";
-    minesweeper.inputs.nix-appimage.follows = "nix-appimage";
 
     # neovim
     nixCats.url = "github:BirdeeHub/nixCats-nvim";
