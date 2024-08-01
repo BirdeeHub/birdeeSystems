e8d192a86a61e448608453a400a04bf1dce660aa
BirdeeHub <robindbor@gmail.com>
2024-08-01 03:45:57 -0700

    updates

--- overlays/nops/default.nix
+++ overlays/nops/default.nix
@@ -9,13 +9,13 @@ importName: inputs:
         })
       ];
     };
-    nops = { lib, writeShellScriptBin, manix, gnused, gnugrep, fzf, findutils }:
+    manixFZF = { lib, writeShellScriptBin, manix, gnused, gnugrep, fzf, findutils }:
       writeShellScriptBin importName ''
         export PATH="${lib.makeBinPath [ manix gnused gnugrep fzf findutils ]}:$PATH"
         manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
       '';
   in {
-    ${importName} = final.callPackage nops { };
+    ${importName} = final.callPackage manixFZF { };
     inherit manix;
   }
 )
