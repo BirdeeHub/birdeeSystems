069c6fb010bdb69c6c780027473d464a0ecd48ac
BirdeeHub <robindbor@gmail.com>
2024-08-01 02:15:15 -0700

    updates

--- overlays/nops/default.nix
+++ overlays/nops/default.nix
@@ -1,6 +1,20 @@
-importName: inputs: (self: super: let
-  pkgs = import inputs.nixpkgsNV { inherit (self) system; };
+importName: inputs: (final: prev: let
+  manix = inputs.manix.packages.${prev.system}.manix.overrideAttrs (fAttrs: pAttrs: {
+    patches = [
+      (prev.substituteAll {
+        src = ./patchNOPSforFLAKE.diff;
+        homeManager = "${inputs.home-manager}";
+      })
+    ];
+  });
+  nopsPKG = { lib, writeShellScriptBin, manix, gnused, coreutils, gnugrep, fzf, findutils, ... }:
+  writeShellScriptBin "nops" (let
+    procPath = [ manix gnused coreutils gnugrep fzf findutils ];
+  in /*bash*/''
+    export PATH="${lib.makeBinPath procPath}:$PATH"
+    manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
+  '');
 in {
-  ${importName} = pkgs.callPackage ./nops.nix { inherit (inputs) home-manager; manix = inputs.manix.packages.${self.system}.manix; };
-  manix = inputs.manix.packages.${self.system}.manix;
+  ${importName} = prev.callPackage nopsPKG { inherit (inputs) home-manager; inherit manix; };
+  inherit manix;
 })
