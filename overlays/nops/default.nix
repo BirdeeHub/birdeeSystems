a0e365b54122350613f719793327509902482b28
BirdeeHub <robindbor@gmail.com>
2024-08-01 03:13:32 -0700

    updates

--- overlays/nops/default.nix
+++ overlays/nops/default.nix
@@ -8,9 +8,9 @@ importName: inputs: (final: prev: let
     ];
   };
   nopsPKG = { lib, writeShellScriptBin, manix, gnused, coreutils, gnugrep, fzf, findutils, ... }:
-  writeShellScriptBin "nops" (let
-    procPath = [ manix gnused coreutils gnugrep fzf findutils ];
-  in /*bash*/''
+    writeShellScriptBin "nops" (let
+      procPath = [ manix gnused coreutils gnugrep fzf findutils ];
+    in /*bash*/''
     export PATH="${lib.makeBinPath procPath}:$PATH"
     manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
   '');
