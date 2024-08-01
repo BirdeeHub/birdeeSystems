9eb2695f464050b642871e3e088716f194a75c10
BirdeeHub <robindbor@gmail.com>
2024-08-01 03:26:31 -0700

    updates

--- overlays/nops/default.nix
+++ overlays/nops/default.nix
@@ -1,20 +1,32 @@
-importName: inputs: (final: prev: let
-  manix = inputs.manix.packages.${prev.system}.manix.overrideAttrs {
-    patches = [
-      (prev.substituteAll {
-        src = ./patchNOPSforFLAKE.diff;
-        homeManager = "${inputs.home-manager}";
-      })
-    ];
-  };
-  nops = { lib, writeShellScriptBin, manix, gnused, coreutils, gnugrep, fzf, findutils, ... }:
-    writeShellScriptBin "nops" (let
-      procPath = [ manix gnused coreutils gnugrep fzf findutils ];
-    in /*bash*/''
-    export PATH="${lib.makeBinPath procPath}:$PATH"
-    manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
-  '');
-in {
-  ${importName} = final.callPackage nops { };
-  inherit manix;
-})
+importName: inputs:
+(
+  final: prev:
+  let
+    manix = inputs.manix.packages.${prev.system}.manix.overrideAttrs {
+      patches = [
+        (prev.substituteAll {
+          src = ./patchNOPSforFLAKE.diff;
+          homeManager = "${inputs.home-manager}";
+        })
+      ];
+    };
+    nops = {
+        lib,
+        writeShellScriptBin,
+        manix,
+        gnused,
+        coreutils,
+        gnugrep,
+        fzf,
+        findutils,
+        ...
+      }: writeShellScriptBin "nops" ''
+        export PATH="${lib.makeBinPath [ manix gnused coreutils gnugrep fzf findutils ]}:$PATH"
+        manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
+      '';
+  in
+  {
+    ${importName} = final.callPackage nops { };
+    inherit manix;
+  }
+)
