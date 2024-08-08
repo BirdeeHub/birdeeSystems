d93691a33577d18c10aa424408e9d0aa96a35239
BirdeeHub <robindbor@gmail.com>
2024-08-08 04:51:56 -0700

    updates

--- templates/flakescript/flake.nix
+++ templates/flakescript/flake.nix
@@ -3,12 +3,10 @@
     forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
     APPNAME = "MyScript";
   in {
-    overlays.default = final: prev: let
-      pkgs = import nixpkgs { inherit (prev) system; };
-    in {
-      ${APPNAME} = pkgs.writeShellScriptBin "${APPNAME}" ''
-          echo "Running '${APPNAME}'..."
-        '';
+    overlays.default = final: prev: {
+      ${APPNAME} = prev.writeShellScriptBin "${APPNAME}" ''
+        echo "Running '${APPNAME}'..."
+      '';
     };
     packages = forAllSys (system: let
       pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
