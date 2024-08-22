175a83978049d7d4c3b8a5b094a5e530a02eafc5
BirdeeHub <robindbor@gmail.com>
2024-08-22 14:19:28 -0700

    eppic fail JKtheSlacker
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/lua/birdee/plugins/completion.lua
    	new file:   common/birdeevim/lua/birdee/snippets.lua

--- common/birdeevim/lua/birdee/plugins/completion.lua
+++ common/birdeevim/lua/birdee/plugins/completion.lua
@@ -69,17 +69,7 @@ require('lze').load {
       require('luasnip.loaders.from_vscode').lazy_load()
       luasnip.config.setup {}
 
-      local ls = require('luasnip')
-      local s = ls.snippet
-      local t = ls.text_node
-      local i = ls.insert_node
-      local extras = require('luasnip.extras')
-      local rep = extras.rep
-      local fmta = require('luasnip.extras.fmt').fmta
-      local c = ls.choice_node
-      local f = ls.function_node
-      local d = ls.dynamic_node
-      local sn = ls.snippet_node
+      require('birdee.snippets')
 
       vim.keymap.set({ "i", "s" }, "<M-n>", function()
           if ls.choice_active() then
