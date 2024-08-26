1a08fcfbc7d27481c4388148b892cf4841833978
BirdeeHub <robindbor@gmail.com>
2024-08-26 01:05:39 -0700

    include shit
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/lua/birdee/plugins/init.lua

--- common/birdeevim/lua/birdee/plugins/init.lua
+++ common/birdeevim/lua/birdee/plugins/init.lua
@@ -252,6 +252,8 @@ require('lze').load {
     enabled = catUtils.enableForCategory('general.other'),
     cmd = { "StartupTime" },
     before = function(_)
+      vim.g.startuptime_event_width = 0
+      vim.g.startuptime_tries = 10
       vim.g.startuptime_exe_path = require("nixCatsUtils").packageBinPath
     end,
   },
