0605818614b2a361be9d44b0f3157ba6ea796120
BirdeeHub <robindbor@gmail.com>
2024-08-30 12:44:59 -0700

    fixed the israeli-palestinian conflict
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/lua/birdee/plugins/completion.lua
    	modified:   common/birdeevim/lua/birdee/snippets.lua

--- common/birdeevim/lua/birdee/plugins/completion.lua
+++ common/birdeevim/lua/birdee/plugins/completion.lua
@@ -54,23 +54,9 @@ return {
   },
   {
     "luasnip",
-    -- cmd = { "" },
-    -- ft = "",
-    -- keys = "",
-    -- colorscheme = "",
     dep_of = { "nvim-cmp" },
     after = function (plugin)
-      local luasnip = require 'luasnip'
-      require('luasnip.loaders.from_vscode').lazy_load()
-      luasnip.config.setup {}
-
       require('birdee.snippets')
-
-      vim.keymap.set({ "i", "s" }, "<M-n>", function()
-          if ls.choice_active() then
-              ls.change_choice(1)
-          end
-      end)
     end,
   },
   {
--- common/birdeevim/lua/birdee/snippets.lua
+++ common/birdeevim/lua/birdee/snippets.lua
@@ -1,3 +1,7 @@
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
 local ls = require('luasnip')
 local s = ls.snippet
 local t = ls.text_node
@@ -9,3 +13,9 @@ local c = ls.choice_node
 local f = ls.function_node
 local d = ls.dynamic_node
 local sn = ls.snippet_node
+
+vim.keymap.set({ "i", "s" }, "<M-n>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)
