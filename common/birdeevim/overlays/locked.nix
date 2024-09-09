6b11f0e5d4968b0c4c6f4ddff085bf97ba3a6699
BirdeeHub <robindbor@gmail.com>
2024-09-08 20:35:13 -0700

    Peopleware Chapter 8: "You Never Get Anything Done around Here between 9 and 5."
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/overlays/locked.nix
    	modified:   flake.lock

--- common/birdeevim/overlays/locked.nix
+++ common/birdeevim/overlays/locked.nix
@@ -2,8 +2,8 @@ importName: inputs: let
   overlay = self: super: let
     pkgs = import inputs.nixpkgsLocked {  inherit (self) system; };
   in { 
-    cpplint = pkgs.cpplint;
-    ueberzugpp = pkgs.ueberzugpp;
+    # cpplint = pkgs.cpplint;
+    # ueberzugpp = pkgs.ueberzugpp;
   };
 in
 overlay
