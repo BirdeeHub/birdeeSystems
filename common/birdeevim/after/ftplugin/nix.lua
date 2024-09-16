a6340ba72c6f835705970149dbdb0af0d6c18996
BirdeeHub <robindbor@gmail.com>
2024-09-16 03:35:36 -0700

    Definitely fixing a mistake Copilot made. Totally not mine.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/after/ftplugin/nix.lua

--- common/birdeevim/after/ftplugin/nix.lua
+++ common/birdeevim/after/ftplugin/nix.lua
@@ -1,4 +1,5 @@
-if nixCats('otter') then
+if false and nixCats('otter') then
+  -- currently disabled because I found it annoying with all the bash errors
   vim.schedule(function ()
     require('otter').activate(nil, true, true, nil)
   end)
