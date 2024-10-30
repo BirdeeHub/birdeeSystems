53508805d41d7dd9a8fe2e7f23c3fe5cbd76d069
BirdeeHub <robindbor@gmail.com>
2024-10-30 09:33:21 -0700

    it's friday
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   common/util/backup_rotator.nix
    	modified:   common/util/default.nix
    	new file:   common/util/mkLuaStuff.nix

---
+++ common/util/backup_rotator.nix
@@ -0,0 +1,50 @@
+with builtins; {
+  writeShellScript
+  , coreutils
+  , SCRIPTNAME ? "backup_rotator"
+  , MOST_RECENT ? "/backup/BACKUPDUMP.zip"
+  , CACHEDIR ? "/backup/backupcache"
+  , dumpAction ? (writeShellScript "dump" ''
+      dest="$1"
+      mkdir -p "$(dirname "$dest")"
+      cp -r "$2" "$dest"
+    '')
+  , max ? 5
+  , permissions ? { dir = 700; file = 600; }
+  , ...
+}:
+writeShellScript SCRIPTNAME (let
+  file_perms = toString permissions.file;
+  dir_perms = toString permissions.dir;
+in /*bash*/''
+  export PATH="${coreutils}/bin:$PATH";
+  umask 077
+  MOST_RECENT='${MOST_RECENT}'
+  CACHEDIR='${CACHEDIR}'
+  cleanup() {
+    [[ -d '${MOST_RECENT}' ]] && chmod ${dir_perms} '${MOST_RECENT}'
+    [[ -f '${MOST_RECENT}' ]] && chmod ${file_perms} '${MOST_RECENT}'
+    [[ -e '${CACHEDIR}' ]] && {
+      chmod -R ${dir_perms} '${CACHEDIR}'
+      find '${CACHEDIR}' -type f -exec chmod ${file_perms} {} \;
+    }
+  }
+  trap cleanup EXIT
+  rotate_mv() {
+    local file=$1 base=$2 zero=$3 num=$4
+    max=${toString max}
+    if [[ $num -gt $max ]]; then
+      [[ $num -gt $zero ]] && rm -r "$file"
+    elif [ -e "$base$num" ]; then
+      rotate_mv "$base$num" "$base" "$zero" "$((num+1))"
+      mv "$file" "$base$num"
+    else
+      mv "$file" "$base$num"
+    fi
+  }
+  if [ -e "$MOST_RECENT" ]; then
+    mkdir -p "$CACHEDIR"
+    rotate_mv "$MOST_RECENT" "$CACHEDIR/$(basename "$MOST_RECENT")." 1 1
+  fi
+  ${dumpAction} "$MOST_RECENT" "$@"
+'')
