1f35221b362d715d40b6bf36dada17c6c5089116
birdee <birdee@birdee.com>
2024-11-05 16:49:47 -0800

    Never gonna give you up
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   systems/PCs/lenny/default.nix

--- systems/PCs/lenny/default.nix
+++ systems/PCs/lenny/default.nix
@@ -32,23 +32,6 @@ in {
       export FLAKE="${flake-path}";
       exec ${self}/scripts/both "$@"
     ''}'';
-    leftMon = ''${pkgs.writeScript "leftMonFlexible.sh" (/*bash*/''
-      #!/usr/bin/env bash
-      rate=59.95; mode="1920x1080"; side="l";
-      current="HDMI-1"; primary="eDP-1";
-      [[ $# > 0 ]] && rate=$1 && shift 1
-      [[ $# > 0 ]] && mode=$1 && shift 1
-      [[ $# > 0 ]] && side=$1 && shift 1
-      [[ $# > 0 ]] && current=$1 && shift 1
-      [[ $# > 0 ]] && primary=$1 && shift 1
-      if [[ $side == "l" ]]; then
-        side="--left-of"
-      elif [[ $side == "r" ]]; then
-        side="--right-of"
-      fi
-      xrandr --output $current $side $primary --rate $rate --mode $mode $@
-    '')}'';
-    leftMonPrf = /*bash*/ "xrandr --output HDMI-1 --left-of eDP-1 --preferred";
   };
 
   # Enable OpenGL
