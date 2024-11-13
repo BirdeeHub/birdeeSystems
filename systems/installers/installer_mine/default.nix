d20518be4c7fe7a1fb17ecdf855fc6f6affcb216
birdee <birdee@birdee.com>
2024-11-13 00:24:53 -0800

    who hurt you
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   systems/installers/installer_mine/default.nix

--- systems/installers/installer_mine/default.nix
+++ systems/installers/installer_mine/default.nix
@@ -37,6 +37,15 @@ in {
     bash.enable = true;
   });
 
+  nix.settings = {
+    extra-trusted-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     inputs.disko.packages.${system}.default
     final_tmux
