04f3e8976a6d021ed60e98cf7823682ddfc00c11
birdee <birdee@birdee.com>
2024-11-29 21:33:31 -0800

    new lenny

--- homes/monitors_by_hostname/nestOS/XmonBoot.sh
+++ homes/monitors_by_hostname/nestOS/XmonBoot.sh
@@ -1,5 +1,5 @@
-xrandr --output DP-2 --primary --preferred
-xrandr --output HDMI-0 --left-of DP-2 --preferred
+# xrandr --output DP-2 --primary --preferred
+# xrandr --output HDMI-0 --left-of DP-2 --preferred
 # xrandr --newmode "1920x1080_custom" 130.18 1920 2008 2052 2200 1080 1084 1089 1125 +hsync +vsync
 # xrandr --addmode HDMI-1-1 "1920x1080_custom"
 # xrandr --output HDMI-1-1 --left-of eDP-1-1 --mode "1920x1080_custom"
