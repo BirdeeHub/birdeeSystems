20b92cffd9470a91e04455baec0eed62a888e3c2
BirdeeHub <birdee@localhost>
2025-02-05 13:20:43 -0800

    For great justice.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	renamed:    common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/mergeable.lua -> common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/merge.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/merge.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/merge.lua
@@ -3,8 +3,10 @@
 ---@field opts? boolean
 
 -- TODO: use modify to bounce everything mergeable
+-- by setting enabled = false
 -- hold them here until trigger is called
 -- merge duplicates coming in.
+-- when trigger is called, lze.load all at once
 
 ---@type table<string, lze.Pluginext>
 local states = {}
@@ -13,6 +15,12 @@ local M = {
   trgger = function() end,
   ---@type lze.Handler
   handler = {
+    spec_field = "merge",
+    -- modify is only called when a plugin's field is not nil
+    modify = function(plugin)
+      plugin.enabled = false
+      return plugin
+    end
   }
 }
 
