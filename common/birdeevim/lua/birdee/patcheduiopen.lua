eb78070f42b4bf1504326515c3e5d6d5f8489422
BirdeeHub <birdee@localhost>
2025-02-04 19:35:23 -0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/lua/birdee/patcheduiopen.lua

--- common/birdeevim/lua/birdee/patcheduiopen.lua
+++ common/birdeevim/lua/birdee/patcheduiopen.lua
@@ -8,35 +8,35 @@ vim.ui.open = function(path)
   local is_repo = vim.bo.filetype == 'lua' and path:match('%w/%w') and vim.fn.count(path, '/') == 1
   local is_dir = path:match('/%w')
   if not is_uri then
-	if is_half_url then
-	  path = ('https://%s'):format(path)
-	elseif is_repo then
-	  path = ('https://github.com/%s'):format(path)
-	elseif not is_dir then
-	  path = ('https://google.com/search?q=%s'):format(path)
-	else
-	  path = vim.fs.normalize(path)
-	end
+    if is_half_url then
+      path = ('https://%s'):format(path)
+    elseif is_repo then
+      path = ('https://github.com/%s'):format(path)
+    elseif not is_dir then
+      path = ('https://google.com/search?q=%s'):format(path)
+    else
+      path = vim.fs.normalize(path)
+    end
   end
 
   local cmd --- @type string[]
 
   if vim.fn.has('mac') == 1 then
-	cmd = { 'open', path }
+    cmd = { 'open', path }
   elseif vim.fn.has('win32') == 1 then
-	if vim.fn.executable('rundll32') == 1 then
-	  cmd = { 'rundll32', 'url.dll,FileProtocolHandler', path }
-	else
-	  return nil, 'vim.ui.open: rundll32 not found'
-	end
+    if vim.fn.executable('rundll32') == 1 then
+      cmd = { 'rundll32', 'url.dll,FileProtocolHandler', path }
+    else
+      return nil, 'vim.ui.open: rundll32 not found'
+    end
   elseif vim.fn.executable('wslview') == 1 then
-	cmd = { 'wslview', path }
+    cmd = { 'wslview', path }
   elseif vim.fn.executable('explorer.exe') == 1 then
-	cmd = { 'explorer.exe', path }
+    cmd = { 'explorer.exe', path }
   elseif vim.fn.executable('xdg-open') == 1 then
-	cmd = { 'xdg-open', path }
+    cmd = { 'xdg-open', path }
   else
-	return nil, 'vim.ui.open: no handler found (tried: wslview, explorer.exe, xdg-open)'
+    return nil, 'vim.ui.open: no handler found (tried: wslview, explorer.exe, xdg-open)'
   end
 
   return vim.system(cmd, { text = true, detach = true }), nil
