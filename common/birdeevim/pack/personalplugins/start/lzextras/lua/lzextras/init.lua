aac39bd7fd939beb0796d821eef3bdbb1941537d
BirdeeHub <birdee@localhost>
2025-02-05 12:26:19 -0800

    I will run 'go fmt' before committing.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
@@ -1,8 +1,13 @@
+---@class lzextras.Debug
+---@field handler lze.Handler
+---@field get_all_plugins fun(): any
+---@field get_a_plugin fun(string): any
+
 ---@class lzextras
----@field key2spec fun(): any
----@field keymap fun(): any
----@field debug fun(): any
----@field make_load_with_afters fun(): any
+---@field key2spec fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts): lze.KeysSpec
+---@field keymap fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts)
+---@field debug lzextras.Debug
+---@field make_load_with_afters (fun(dirs: string[]|string): fun(names: string|string[]))|(fun(dirs: string[]|string, load: fun(name: string):string|nil): fun(names: string|string[]))
 local lzextras = {}
 
 return setmetatable(lzextras,{
