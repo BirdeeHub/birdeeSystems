21e9889fa6fcaf61741d6e7962f23075e9e718b7
BirdeeHub <birdee@localhost>
2025-02-05 11:52:38 -0800

    Who knows...
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	renamed:    common/birdeevim/pack/personalplugins/opt/visual_surrounds/plugin/init.lua -> common/birdeevim/plugin/misc.lua

--- common/birdeevim/plugin/misc.lua
+++ common/birdeevim/plugin/misc.lua
@@ -1,3 +1,3 @@
 -- TODO: make this into a keybind with operator pending to select text to surround with,
 -- like vim-surrounds but for visual selection instead of motion
-vim.keymap.set({'v', 'x'}, "<leader>s", [[:'<,'>s/\%V\(.*\)\%V/<before-text>\1<after-text>/]], { desc = 'Replace inside selection with text' })
+-- vim.keymap.set({'v', 'x'}, "<leader>s", [[:'<,'>s/\%V\(.*\)\%V/<before-text>\1<after-text>/]], { desc = 'Replace inside selection with text' })
