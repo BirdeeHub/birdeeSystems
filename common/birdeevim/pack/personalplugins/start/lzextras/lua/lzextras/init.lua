1079685c48a9bce44ed311772f1bf90fc748ae8d
BirdeeHub <birdee@localhost>
2025-02-05 12:29:04 -0800

    TODO: Replace placeholder code
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
@@ -8,6 +8,8 @@
 ---@field keymap fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts)
 ---@field debug lzextras.Debug
 ---@field make_load_with_afters (fun(dirs: string[]|string): fun(names: string|string[]))|(fun(dirs: string[]|string, load: fun(name: string):string|nil): fun(names: string|string[]))
+
+---@type lzextras
 local lzextras = {}
 
 return setmetatable(lzextras,{
