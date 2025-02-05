024da22345878fe94f336224f6127df59af49d38
BirdeeHub <birdee@localhost>
2025-02-05 12:01:07 -0800

    assorted changes
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
    	new file:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/lib/key2spec.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.keymap = require("lzextras.lib.keymap")
+M.key2spec = require("lzextras.lib.key2spec")
 M.debug = require("lzextras.h.debug")
 M.load_with_afters = require("lzextras.lib.load_with_afters")
 
