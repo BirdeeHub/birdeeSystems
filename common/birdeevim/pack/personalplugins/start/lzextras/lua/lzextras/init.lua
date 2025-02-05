4faac04d61311c75918802246ffcb56d8f916b23
BirdeeHub <birdee@localhost>
2025-02-05 13:47:26 -0800

    bad things happen when you forget about that one little change you made ages ago
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/init.lua
@@ -2,9 +2,12 @@
 ---@field handler lze.Handler
 ---@field trigger fun()
 
+---@class lzextras.Keymap
+---@field set fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts)
+
 ---@class lzextras
 ---@field key2spec fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts): lze.KeysSpec
----@field keymap fun(mode:string|string[], lhs:string, rhs:string|function, opts:vim.keymap.set.Opts)
+---@field keymap fun(plugin: string|lze.PluginSpec): lzextras.Keymap
 ---@field make_load_with_afters (fun(dirs: string[]|string): fun(names: string|string[]))|(fun(dirs: string[]|string, load: fun(name: string):string|nil): fun(names: string|string[]))
 ---@field merge lzextras.Merge
 
