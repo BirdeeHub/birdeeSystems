c6a4af6f43b36f9f35f1b4ffca58cc29e7abf318
BirdeeHub <birdee@localhost>
2025-02-05 08:46:46 -0800

    cmon... Why...

--- common/birdeevim/lua/birdee/plugins/snacks.lua
+++ common/birdeevim/lua/birdee/plugins/snacks.lua
@@ -1,60 +1,78 @@
+-- NOTE: this plugin is "loaded" at startup,
+-- but we delay the main setup call until later.
+-- also we still need to require bigfile from the main one
+-- because it tries to index the Snacks global unsafely...
 
--- NOTE: It is faster when you comment them out
--- rather than disabling them.
--- for some reason, they are still required
--- when you do { enabled = false }
-require('snacks').setup({
-  -- dashboard = { enabled = true, },
-  -- debug = { enabled = true, },
-  -- bufdelete = { enabled = true, },
-  -- dim = { enabled = true, },
-  -- explorer = { enabled = true, },
-  -- quickfile = { enabled = true, },
-  -- input = { enabled = true, },
-  -- scratch = { enabled = true, },
-  -- layout = { enabled = true, },
-  -- zen = { enabled = true, },
-  -- scroll = { enabled = true, },
-  -- notifier = { enabled = true, },
-  -- notify = { enabled = true, },
-  -- win = { enabled = true, },
-  -- picker = { enabled = true, },
-  -- profiler = { enabled = true, },
-  -- toggle = { enabled = true, },
-  -- rename = { enabled = true, },
-  -- words = { enabled = true, },
+-- also shut up I dont care
+---@diagnostic disable-next-line: invisible
+require('snacks').bigfile.setup()
 
-  gitbrowse = { enabled = true, },
-  lazygit = { enabled = true, },
-  bigfile = { enabled = true, },
-  git = { enabled = true, },
-  terminal = { enabled = true, },
-  scope = { enabled = true, },
-  indent = {
-    enabled = true,
-    scope = {
-      hl = 'Hlargs',
+return {
+  {
+    "snacks.nvim",
+    keys = {
+      {'<c-\\>', function() Snacks.terminal() end, mode = {'n'}, desc = 'open snacks terminal' },
+      {"<leader>_", function() Snacks.lazygit.open() end, mode = {"n"}, desc = 'LazyGit' },
+      {"<leader>gc", function() Snacks.lazygit.log() end, mode = {"n"}, desc = 'Lazy[G]it [C]ommit log' },
+      {"<leader>gl", function() Snacks.gitbrowse.open() end, mode = {"n"}, desc = '[G]oto git [L]ink' },
     },
-    chunk = {
-      -- enabled = true,
-      hl = 'Hlargs',
-    }
-  },
-  statuscolumn = {
-    left = { "mark", "git" }, -- priority of signs on the left (high to low)
-    right = { "fold", "sign" }, -- priority of signs on the right (high to low)
-    folds = {
-      open = false, -- show open fold icons
-      git_hl = false, -- use Git Signs hl for fold icons
-    },
-    git = {
-      -- patterns to match Git signs
-      patterns = { "GitSign", "MiniDiffSign" },
-    },
-    refresh = 50, -- refresh at most every 50ms
-  },
-})
-vim.keymap.set({'n'}, '<c-\\>', function() Snacks.terminal() end, { desc = 'open snacks terminal' })
-vim.keymap.set({"n"},"<leader>_", function() Snacks.lazygit.open() end, { desc = 'LazyGit' })
-vim.keymap.set({"n"},"<leader>gc", function() Snacks.lazygit.log() end, { desc = 'Lazy[G]it [C]ommit log' })
-vim.keymap.set({"n"},"<leader>gl", function() Snacks.gitbrowse.open() end, { desc = '[G]oto git [L]ink' })
+    event = { 'DeferredUIEnter' },
+    after = function(_)
+      -- NOTE: It is faster when you comment them out
+      -- rather than disabling them.
+      -- for some reason, they are still required
+      -- when you do { enabled = false }
+      require('snacks').setup({
+        -- dashboard = { enabled = true, },
+        -- debug = { enabled = true, },
+        -- bufdelete = { enabled = true, },
+        -- dim = { enabled = true, },
+        -- explorer = { enabled = true, },
+        -- quickfile = { enabled = true, },
+        -- bigfile = { enabled = true, },
+        -- input = { enabled = true, },
+        -- scratch = { enabled = true, },
+        -- layout = { enabled = true, },
+        -- zen = { enabled = true, },
+        -- scroll = { enabled = true, },
+        -- notifier = { enabled = true, },
+        -- notify = { enabled = true, },
+        -- win = { enabled = true, },
+        -- picker = { enabled = true, },
+        -- profiler = { enabled = true, },
+        -- toggle = { enabled = true, },
+        -- rename = { enabled = true, },
+        -- words = { enabled = true, },
+
+        gitbrowse = { enabled = true, },
+        lazygit = { enabled = true, },
+        git = { enabled = true, },
+        terminal = { enabled = true, },
+        scope = { enabled = true, },
+        indent = {
+          enabled = true,
+          scope = {
+            hl = 'Hlargs',
+          },
+          chunk = {
+            -- enabled = true,
+            hl = 'Hlargs',
+          }
+        },
+        statuscolumn = {
+          left = { "mark", "git" }, -- priority of signs on the left (high to low)
+          right = { "fold", "sign" }, -- priority of signs on the right (high to low)
+          folds = {
+            open = false, -- show open fold icons
+            git_hl = false, -- use Git Signs hl for fold icons
+          },
+          git = {
+            -- patterns to match Git signs
+            patterns = { "GitSign", "MiniDiffSign" },
+          },
+          refresh = 50, -- refresh at most every 50ms
+        },
+      })
+    end,
+  }
+}
