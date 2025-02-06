da9d881a8a4cb375575900322b5f35cf2c2e28f2
BirdeeHub <birdee@localhost>
2025-02-05 16:16:12 -0800

    This is the last time we let Dimitris commit ascii porn in the comments.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua
@@ -1,54 +1,36 @@
 ---@class lzextras.LspPlugin: lze.Plugin
 ---@field lsp? any
----@field after? table|function
-
----@type table<string, lzextras.LspPlugin[]>
-local states = {}
 
 -- TODO: make a handler for lspconfig specs
 local M = {
   ---@type lze.Handler
   handler = {
     spec_field = "lsp",
-    -- modify is only called when a plugin's field is not nil
-    -- modify should return a plugin where the after field is replaced
     ---@param plugin lzextras.LspPlugin
     modify = function(plugin)
-      if not plugin.lsp then
+      local lspfield = plugin.lsp
+      if not lspfield then
         return plugin
       end
-      if type(plugin.after) ~= "table" then
-        vim.notify('lsp spec for "' .. plugin.name .. '" failed, after field must be a table',
+      if type(lspfield) ~= "table" then
+        vim.notify('lsp spec for "' .. plugin.name .. '" failed, lsp field must be a table',
           vim.log.levels.ERROR, { title = "lzextras.lsp" })
         return plugin
       end
-      local old_after = plugin.after or {}
-      plugin.after = function (p)
-        require('lspconfig')[p.name].setup(old_after)
+      plugin.load = function(name)
+        require('lspconfig')[name].setup(lspfield)
       end
-      local oldfttype = type(old_after.filetypes)
+      local ftlist = lspfield.filetypes
+      local oldfttype = type(ftlist)
       if oldfttype == "string" then
-        plugin.ft = old_after.filetypes
-      elseif oldfttype == "table" and #old_after.filetypes > 0 then
-        plugin.ft = old_after.filetypes
+        plugin.ft = ftlist
+      elseif oldfttype == "table" and #ftlist > 0 then
+        plugin.ft = ftlist
       else
         vim.notify('lsp spec for "' .. plugin.name .. '" failed, no filetypes specified',
           vim.log.levels.ERROR, { title = "lzextras.lsp" })
       end
       return plugin
-    end,
-    ---@param plugin lzextras.LspPlugin
-    before = function(plugin)
-      if not plugin.lsp then
-        return
-      end
-      for _, filetypelist in pairs(states) do
-        for i = #filetypelist, 1, -1 do
-          if filetypelist[i].name == plugin.name then
-            table.remove(filetypelist, i)
-          end
-        end
-      end
     end,
   },
 }
