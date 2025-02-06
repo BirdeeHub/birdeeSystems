46334a704bc25785ba56601875ecf87b9b174733
BirdeeHub <birdee@localhost>
2025-02-05 16:04:19 -0800

    Glue. Match sticks. Paper. Build script!
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua

--- common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua
+++ common/birdeevim/pack/personalplugins/start/lzextras/lua/lzextras/src/lsp.lua
@@ -1,5 +1,6 @@
 ---@class lzextras.LspPlugin: lze.Plugin
 ---@field lsp? any
+---@field after? table|function
 
 ---@type table<string, lzextras.LspPlugin[]>
 local states = {}
@@ -10,18 +11,31 @@ local M = {
   handler = {
     spec_field = "lsp",
     -- modify is only called when a plugin's field is not nil
+    -- modify should return a plugin where the after field is replaced
     ---@param plugin lzextras.LspPlugin
     modify = function(plugin)
       if not plugin.lsp then
         return plugin
       end
-      return plugin
-    end,
-    ---@param plugin lzextras.LspPlugin
-    add = function(plugin)
-      if not plugin.lsp then
-        return
+      if type(plugin.after) ~= "table" then
+        vim.notify('lsp spec for "' .. plugin.name .. '" failed, after field must be a table',
+          vim.log.levels.ERROR, { title = "lzextras.lsp" })
+        return plugin
       end
+      local old_after = plugin.after or {}
+      plugin.after = function (p)
+        require('lspconfig')[p.name].setup(old_after)
+      end
+      local oldfttype = type(old_after.filetypes)
+      if oldfttype == "string" then
+        plugin.ft = old_after.filetypes
+      elseif oldfttype == "table" and #old_after.filetypes > 0 then
+        plugin.ft = old_after.filetypes
+      else
+        vim.notify('lsp spec for "' .. plugin.name .. '" failed, no filetypes specified',
+          vim.log.levels.ERROR, { title = "lzextras.lsp" })
+      end
+      return plugin
     end,
     ---@param plugin lzextras.LspPlugin
     before = function(plugin)
