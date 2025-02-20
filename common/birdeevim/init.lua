5493d5365a44d789606b52a26b0bfe0539eb0e3c
BirdeeHub <birdee@localhost>
2025-02-20 03:21:28 -0800

    fix bug, for realz
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/init.lua
    	renamed:    common/birdeevim/lua/birdee/cfg/clippy.lua -> common/birdeevim/plugin/clippy.lua
    	renamed:    common/birdeevim/lua/birdee/cfg/keymaps.lua -> common/birdeevim/plugin/keymaps.lua
    	renamed:    common/birdeevim/lua/birdee/cfg/opts.lua -> common/birdeevim/plugin/opts.lua
    	renamed:    common/birdeevim/lua/birdee/cfg/patcheduiopen.lua -> common/birdeevim/plugin/patcheduiopen.lua

--- common/birdeevim/init.lua
+++ common/birdeevim/init.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 require('nixCatsUtils').setup { non_nix_value = true }
-require("birdee.cfg.patcheduiopen")
-require("birdee.cfg.opts")
-require("birdee.cfg.keymaps")
-require("birdee.cfg.clippy")
 if vim.g.vscode == nil then
   _G.lze = require('lze')
   lze.register_handlers {
