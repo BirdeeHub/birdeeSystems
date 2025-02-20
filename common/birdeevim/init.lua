4debb88a51160f62de957ab6fb0b82c1d554c427
BirdeeHub <birdee@localhost>
2025-02-20 03:01:19 -0800

    debugo
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/init.lua
    	renamed:    common/birdeevim/lua/birdee/clippy.lua -> common/birdeevim/lua/birdee/cfg/clippy.lua
    	renamed:    common/birdeevim/lua/birdee/keymaps.lua -> common/birdeevim/lua/birdee/cfg/keymaps.lua
    	renamed:    common/birdeevim/lua/birdee/opts.lua -> common/birdeevim/lua/birdee/cfg/opts.lua
    	renamed:    common/birdeevim/lua/birdee/patcheduiopen.lua -> common/birdeevim/lua/birdee/cfg/patcheduiopen.lua
    	modified:   common/birdeevim/lua/birdee/init.lua

--- common/birdeevim/init.lua
+++ common/birdeevim/init.lua
@@ -1,5 +1,15 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 require('nixCatsUtils').setup { non_nix_value = true }
-_G.lze = require('lze')
-require('birdee')
+require("birdee.cfg.patcheduiopen")
+require("birdee.cfg.opts")
+require("birdee.cfg.keymaps")
+require("birdee.cfg.clippy")
+if vim.g.vscode == nil then
+  _G.lze = require('lze')
+  lze.register_handlers({
+      require("nixCatsUtils.lzUtils").for_cat,
+      require('lzextras').lsp,
+  })
+  require('birdee')
+end
--- common/birdeevim/lua/birdee/init.lua
+++ common/birdeevim/lua/birdee/init.lua
@@ -1,42 +1,25 @@
-require("birdee.patcheduiopen")
-require("birdee.opts")
-require("birdee.keymaps")
-require("birdee.clippy")
-
-if vim.g.vscode == nil then
-  -- local ok, notify = pcall(require, "notify")
-  -- if ok then
-  --   notify.setup({
-  --     on_open = function(win)
-  --       vim.api.nvim_win_set_config(win, { focusable = false })
-  --     end,
-  --   })
-  --   vim.notify = notify
-  --   vim.keymap.set("n", "<Esc>", function()
-  --       notify.dismiss({ silent = true, })
-  --   end, { desc = "dismiss notify popup and clear hlsearch" })
-  -- end
-  -- vim.g.lze = {
-  --   load = vim.cmd.packadd,
-  --   verbose = true,
-  -- }
-  lze.register_handlers({
-    {
-      enabled = true,
-      handler = require("nixCatsUtils.lzUtils").for_cat
-    },
-    {
-      enabled = true,
-      handler = require('lzextras').lsp,
-    },
-  })
-  -- TODO: this in another file and require here.
-  -- require('nixCatsUtils.catPacker').setup({ your plugins })
-  require("birdee.plugins")
-  require("birdee.LSPs")
-  if nixCats('debug') then
-    require("birdee.debug")
-  end
-  require("birdee.format")
-  require("birdee.lint")
+-- local ok, notify = pcall(require, "notify")
+-- if ok then
+--   notify.setup({
+--     on_open = function(win)
+--       vim.api.nvim_win_set_config(win, { focusable = false })
+--     end,
+--   })
+--   vim.notify = notify
+--   vim.keymap.set("n", "<Esc>", function()
+--       notify.dismiss({ silent = true, })
+--   end, { desc = "dismiss notify popup and clear hlsearch" })
+-- end
+-- vim.g.lze = {
+--   load = vim.cmd.packadd,
+--   verbose = true,
+-- }
+-- TODO: this in another file and require here.
+-- require('nixCatsUtils.catPacker').setup({ your plugins })
+require("birdee.plugins")
+require("birdee.LSPs")
+if nixCats('debug') then
+  require("birdee.debug")
 end
+require("birdee.format")
+require("birdee.lint")
