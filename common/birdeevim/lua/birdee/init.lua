ec168b2f17372e9ad4edc9f8a67dea30cc54e99a
BirdeeHub <birdee@localhost>
2025-02-22 01:40:40 -0800

    better ignores
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/birdeevim/lua/birdee/init.lua
    	modified:   common/birdeevim/lua/birdee/plugins/init.lua

--- common/birdeevim/lua/birdee/init.lua
+++ common/birdeevim/lua/birdee/init.lua
@@ -1,27 +1,16 @@
 -- TODO: this in another file and require here.
 -- require('birdee.non_nix_download').setup({ your plugins })
-require('lze').register_handlers {
-    require("nixCatsUtils.lzUtils").for_cat,
-    require('lzextras').lsp,
-}
--- local ok, notify = pcall(require, "notify")
--- if ok then
---   notify.setup({
---     on_open = function(win)
---       vim.api.nvim_win_set_config(win, { focusable = false })
---     end,
---   })
---   vim.notify = notify
---   vim.keymap.set("n", "<Esc>", function()
---       notify.dismiss({ silent = true, })
---   end, { desc = "dismiss notify popup and clear hlsearch" })
--- end
+
 -- vim.g.lze = {
 --   load = vim.cmd.packadd,
 --   verbose = true,
 --   default_priority = 50,
 --   without_default_handlers = false,
 -- }
+require('lze').register_handlers {
+    require("nixCatsUtils.lzUtils").for_cat,
+    require('lzextras').lsp,
+}
 require('lze').load {
   { import = "birdee.plugins" },
   { import = "birdee.LSPs" },
--- common/birdeevim/lua/birdee/plugins/init.lua
+++ common/birdeevim/lua/birdee/plugins/init.lua
@@ -1,3 +1,15 @@
+-- local ok, notify = pcall(require, "notify")
+-- if ok then
+--   notify.setup({
+--     on_open = function(win)
+--       vim.api.nvim_win_set_config(win, { focusable = false })
+--     end,
+--   })
+--   vim.notify = notify
+--   vim.keymap.set("n", "<Esc>", function()
+--       notify.dismiss({ silent = true, })
+--   end, { desc = "dismiss notify popup and clear hlsearch" })
+-- end
 local catUtils = require('nixCatsUtils')
 local colorschemer = nixCats.extra('colorscheme') -- also schemes lualine
 if not catUtils.isNixCats then
