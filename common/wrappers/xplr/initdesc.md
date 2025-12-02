4d6d824a6d0c4ec8fa62aa80ac54bfaa498da69d
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2025-12-02 08:04:10 -0800

    Fixed the flux capacitor again
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/wrappers/xplr/default.nix
    	new file:   common/wrappers/xplr/initdesc.md
    	modified:   common/wrappers/xplr/xplr.nix

---
+++ common/wrappers/xplr/initdesc.md
@@ -0,0 +1,38 @@
+`luaInit` is a flexible configuration option for providing Lua code that will be executed when the Lua environment for `xplr` is initialized.
+
+It can be either a simple string of Lua code or a structured DAG (directed acyclic graph) of Lua code snippets with dependencies between them.
+
+The dag type has an extra `opts` field that can be used to pass in options to the lua code.
+
+It is like the `config.luaInfo` option, but per entry.
+
+You can then recieve it in `.data` with `local opts, name = ...`
+
+`{ data, after ? [], before ? [], opts ? {} }`
+
+**Example usage:**
+
+```nix
+luaEnv = lp: [ lp.inspect ];
+luaInit.WillRunEventually = "print('you can also just put a string if you dont want opts or need to run it before or after another')";
+luaInit.TESTFILE_1 = {
+  opts = { testval = 1; };
+  data = ''
+    local opts, name = ...
+    print(name, require("inspect")(opts))
+    return opts.hooks -- xplr configurations can return hooks
+  '';
+luaInit.TESTFILE_2 = {
+  opts = { testval = 2; };
+  after = [ "TESTFILE_1" ];
+  data = ''
+    local opts, name = ...
+    print(name, require("inspect")(opts))
+    return opts.hooks -- xplr configurations can return hooks
+  '';
+};
+```
+
+Here, `TESTFILE_1` runs before `TESTFILE_2`, with their respective options passed in.
+
+`WillRunEventually` will run at some point, but when is not specified. It could even run between `TESTFILE_1` and `TESTFILE_2`
--- common/wrappers/xplr/xplr.nix
+++ common/wrappers/xplr/xplr.nix
@@ -1,30 +1,50 @@
-{ config, pkgs, lib, wlib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  wlib,
+  ...
+}:
 let
   luaType = (pkgs.formats.lua { }).type;
-  withPackagesType = let
-    inherit (lib.types) package listOf functionTo;
-  in (functionTo (listOf package)) // {
-    merge =
-      loc: defs: arg:
-      (listOf package).merge (loc ++ [ "<function body>" ]) (
-        map (
-          def:
-          def
-          // {
-            value = def.value arg;
-          }
-        ) defs
-      );
-  };
+  withPackagesType =
+    let
+      inherit (lib.types) package listOf functionTo;
+    in
+    (functionTo (listOf package))
+    // {
+      merge =
+        loc: defs: arg:
+        (listOf package).merge (loc ++ [ "<function body>" ]) (
+          map (
+            def:
+            def
+            // {
+              value = def.value arg;
+            }
+          ) defs
+        );
+    };
   configDag = wlib.types.dagOf // {
-    extraOptions.opts = lib.mkOption {
-      type = luaType;
-      default = {};
-      description = ''
-        Can be received in `.data` with `local opts, name = ...`
-      '';
+    extraOptions = {
+      opts = lib.mkOption {
+        type = luaType;
+        default = { };
+        description = ''
+          Can be received in `.data` with `local opts, name = ...`
+        '';
+      };
+      type = lib.mkOption {
+        type = lib.types.enum [
+          "fnl"
+          "lua"
+        ];
+        default = config.defaultConfigLang;
+      };
     };
   };
+  initDal = wlib.dag.sortAndUnwrap { dag = config.luaInit; };
+  hasFnl = builtins.any (v: v.type == "fnl") initDal;
 in
 {
   imports = [ wlib.modules.default ];
@@ -32,6 +52,13 @@ in
     type = lib.types.package;
     default = pkgs.luajit;
     description = "The lua derivation used to evaluate the `luaEnv` option";
+  };
+  options.defaultConfigLang = lib.mkOption {
+    type = lib.types.enum [
+      "fnl"
+      "lua"
+    ];
+    default = "lua";
   };
   options.luaEnv = lib.mkOption {
     type = withPackagesType;
@@ -49,46 +76,7 @@ in
   options.luaInit = lib.mkOption {
     type = lib.types.either lib.types.str (configDag lib.types.lines);
     default = { };
-    description = ''
-      `luaInit` is a flexible configuration option for providing Lua code that will be executed when the Lua environment for `xplr` is initialized.
-
-      It can be either a simple string of Lua code or a structured DAG (directed acyclic graph) of Lua code snippets with dependencies between them.
-
-      The dag type has an extra `opts` field that can be used to pass in options to the lua code.
-
-      It is like the `config.luaInfo` option, but per entry.
-
-      You can then recieve it in `.data` with `local opts, name = ...`
-
-      `{ data, after ? [], before ? [], opts ? {} }`
-
-      **Example usage:**
-
-      ```nix
-      luaEnv = lp: [ lp.inspect ];
-      luaInit.WillRunEventually = "print('you can also just put a string if you dont want opts or need to run it before or after another')";
-      luaInit.TESTFILE_1 = {
-        opts = { testval = 1; };
-        data = ${"''\n    "+''
-          local opts, name = ...
-              print(name, require("inspect")(opts))
-        ''+"  '';"}
-      };
-      luaInit.TESTFILE_2 = {
-        opts = { testval = 2; };
-        after = [ "TESTFILE_1" ];
-        data = ${"''\n    "+''
-          local opts, name = ...
-              print(name, require("inspect")(opts))
-              return opts.hooks -- xplr configurations can return hooks
-        ''+"  '';"}
-      };
-      ```
-
-      Here, `TESTFILE_1` runs before `TESTFILE_2`, with their respective options passed in.
-
-      `WillRunEventually` will run at some point, but when is not specified. It could even run between `TESTFILE_1` and `TESTFILE_2`
-    '';
+    description = builtins.readFile ./initdesc.md;
   };
   options.luaInfo = lib.mkOption {
     type = luaType;
@@ -101,101 +89,141 @@ in
   };
   config.package = lib.mkDefault pkgs.xplr;
   config.drv.passAsFile = [ "nixLuaInit" ];
-  config.drv.nixLuaInfo = /* lua */ ''
-    package.preload["nix-info"] = function()
-      return setmetatable(${lib.generators.toLua { } config.luaInfo}, {
-        __call = function(self, default, ...)
-          if select('#', ...) == 0 then return default end
-          local tbl = self;
-          for _, key in ipairs({...}) do
-            if type(tbl) ~= "table" then return default end
-            tbl = tbl[key]
-          end
-          return tbl
-        end
-      })
-    end
-  '';
-  config.drv.nixLuaInit = let
-    generateConfig = dag: builtins.concatStringsSep ",\n  " (
-      wlib.dag.sortAndUnwrap {
-        inherit dag;
-        mapIfOk = v: "(function(...)\n${v.data}\n  end)(${lib.generators.toLua { } v.opts}, ${builtins.toJSON v.name})";
-      }
-    );
-  in (if builtins.isString config.luaInit then config.luaInit else /*lua*/''
-    return (function(hooks)
-      local function add_hooks(res, b)
-        for k, vlist in pairs(b) do
-          local acc = res[k]
-          if type(acc) ~= "table" then
-            res[k] = vlist
-          elseif type(vlist) ~= "table" then
-            error("expected a list of hooks at ".. tostring(k) ..", but got a " .. type(vlist))
-          else
-            local n = #acc
-            for i = 1, #vlist do
-              acc[n + i] = vlist[i]
-            end
-          end
-        end
-        return res
-      end
-      local result = {}
-      for _, h in ipairs(hooks) do
-        if type(h) == "table" then
-          result = add_hooks(result, h)
-        end
-      end
-      return result
-    end)({
-      ${generateConfig config.luaInit}
-    })
-  '');
-  /**
-    (tset _G :version ${builtins.toJSON config.package.version})
-    ((fn [hooks]
-       (fn add-hooks [res b]
-         (each [k vlist (pairs b)]
-           (local acc (. res k))
-           (if (not= (type acc) :table) (tset res k vlist)
-               (not= (type vlist) :table)
-               (error (.. "expected a list of hooks at " (tostring k)
-                          ", but got a " (type vlist)))
-               (let [n (length acc)]
-                 (for [i 1 (length vlist)] (tset acc (+ n i) (. vlist i))))))
-         res)
+  config.drv.nixLuaInit =
+    let
+      nixLuaInfo =
+        let
+          infoscript = /* lua */ ''
+            package.preload["nix-info"] = function()
+              return setmetatable(${lib.generators.toLua { } config.luaInfo}, {
+                __call = function(self, default, ...)
+                  if select('#', ...) == 0 then return default end
+                  local tbl = self;
+                  for _, key in ipairs({...}) do
+                    if type(tbl) ~= "table" then return default end
+                    tbl = tbl[key]
+                  end
+                  return tbl
+                end
+              })
+            end'';
+        in
+        if hasFnl then "(lua ${builtins.toJSON infoscript})" else infoscript;
+      versionstr =
+        if hasFnl then
+          "(tset _G :version ${builtins.toJSON config.package.version})"
+        else
+          "version = ${builtins.toJSON config.package.version}";
+      generatedConfig = lib.pipe initDal [
+        (map (
+          v:
+          let
+            lua = "(function(...) ${v.data} end)(${lib.generators.toLua { } v.opts}, ${builtins.toJSON v.name})";
+            fnl = ''((fn [...] ${v.data}) (lua "" ${builtins.toJSON "${lib.generators.toLua { } v.opts}"}) ${builtins.toJSON v.name})'';
+          in
+          if hasFnl then if v.type == "fnl" then fnl else ''(lua "" ${builtins.toJSON lua})'' else lua
+        ))
+        (builtins.concatStringsSep (if hasFnl then "\n" else ",\n"))
+      ];
 
-       (var result {})
-       (each [_ h (ipairs hooks)]
-         (when (= (type h) :table) (set result (add-hooks result h))))
-       result) {
-        ${generateConfig config.luaInit}
-       })
-  */
-  config.drv.buildPhase = let
-  in /*bash*/''
+      nixInit = (
+        if builtins.isString config.luaInit then
+          config.luaInit
+        else if hasFnl then
+          /* fennel */ ''
+            ((fn [hooks]
+               (fn add-hooks [res b]
+                 (each [k vlist (pairs b)]
+                   (local acc (. res k))
+                   (if (not= (type acc) :table) (tset res k vlist)
+                       (not= (type vlist) :table)
+                       (error (.. "expected a list of hooks at " (tostring k)
+                                  ", but got a " (type vlist)))
+                       (let [n (length acc)]
+                         (for [i 1 (length vlist)] (tset acc (+ n i) (. vlist i))))))
+                 res)
+
+               (var result {})
+               (each [_ h (ipairs hooks)]
+                 (when (= (type h) :table) (set result (add-hooks result h))))
+               result) [
+                ${generatedConfig}
+               ])''
+        else
+          /* lua */ ''
+            return (function(hooks)
+              local function add_hooks(res, b)
+                for k, vlist in pairs(b) do
+                  local acc = res[k]
+                  if type(acc) ~= "table" then
+                    res[k] = vlist
+                  elseif type(vlist) ~= "table" then
+                    error("expected a list of hooks at ".. tostring(k) ..", but got a " .. type(vlist))
+                  else
+                    local n = #acc
+                    for i = 1, #vlist do
+                      acc[n + i] = vlist[i]
+                    end
+                  end
+                end
+                return res
+              end
+              local result = {}
+              for _, h in ipairs(hooks) do
+                if type(h) == "table" then
+                  result = add_hooks(result, h)
+                end
+              end
+              return result
+            end)({
+              ${generatedConfig}
+            })''
+      );
+    in
+    ''
+      ${versionstr}
+      ${nixLuaInfo}
+      ${nixInit}
+    '';
+  config.drv.buildPhase = ''
     runHook preBuild
-    { [ -e "$nixLuaInitPath" ] && cat "$nixLuaInitPath" || echo "$nixLuaInit"; } > ${lib.escapeShellArg "${placeholder "out"}/${config.binName}-rc.lua"}
+    { [ -e "$nixLuaInitPath" ] && cat "$nixLuaInitPath" || echo "$nixLuaInit"; }${
+      if hasFnl then " | ${pkgs.luajitPackages.fennel}/bin/fennel --compile - " else " "
+    }> ${lib.escapeShellArg "${placeholder "out"}/${config.binName}-rc.lua"}
     runHook postBuild
   '';
-  config.suffixVar = let
-    withPackages = config.lua.withPackages or pkgs.luajit.withPackages;
-    genLuaCPathAbsStr =
-      config.lua.pkgs.luaLib.genLuaCPathAbsStr or pkgs.luajit.pkgs.luaLib.genLuaCPathAbsStr;
-    genLuaPathAbsStr =
-      config.lua.pkgs.luaLib.genLuaPathAbsStr or pkgs.luajit.pkgs.luaLib.genLuaPathAbsStr;
-    luaEnv = withPackages config.luaEnv;
-  in lib.mkIf ((config.luaEnv config.lua.pkgs) != [ ]) [
-    [ "LUA_PATH" ";" (genLuaPathAbsStr luaEnv) ]
-    [ "LUA_CPATH" ";" (genLuaCPathAbsStr luaEnv) ]
+  config.suffixVar =
+    let
+      withPackages = config.lua.withPackages or pkgs.luajit.withPackages;
+      genLuaCPathAbsStr =
+        config.lua.pkgs.luaLib.genLuaCPathAbsStr or pkgs.luajit.pkgs.luaLib.genLuaCPathAbsStr;
+      genLuaPathAbsStr =
+        config.lua.pkgs.luaLib.genLuaPathAbsStr or pkgs.luajit.pkgs.luaLib.genLuaPathAbsStr;
+      luaEnv = withPackages config.luaEnv;
+    in
+    lib.mkIf ((config.luaEnv config.lua.pkgs) != [ ]) [
+      [
+        "LUA_PATH"
+        ";"
+        (genLuaPathAbsStr luaEnv)
+      ]
+      [
+        "LUA_CPATH"
+        ";"
+        (genLuaCPathAbsStr luaEnv)
+      ]
+    ];
+  config.addFlag = [
+    {
+      # use addFlag because it allows multiple -c args
+      # and while config.flags values can be a list for that purpose,
+      # the esc-fn option would be for all of them.
+      name = "GENERATED_WRAPPER_LUA";
+      data = [
+        "-c"
+        "${placeholder "out"}/${config.binName}-rc.lua"
+      ];
+      esc-fn = lib.escapeShellArg;
+    }
   ];
-  config.addFlag = [ {
-    # use addFlag because it allows multiple -c args
-    # and while config.flags values can be a list for that purpose,
-    # the esc-fn option would be for all of them.
-    name = "GENERATED_WRAPPER_LUA";
-    data = [ "-c" "${placeholder "out"}/${config.binName}-rc.lua" ];
-    esc-fn = lib.escapeShellArg;
-  } ];
 }
