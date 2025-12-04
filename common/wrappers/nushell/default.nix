700d00b9d19555d092e19ab1447d8119a17ee992
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2025-12-04 05:51:41 -0800

    Added a feature that makes coffee taste like java.
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/wrappers/nushell/default.nix

--- common/wrappers/nushell/default.nix
+++ common/wrappers/nushell/default.nix
@@ -3,6 +3,6 @@
   "config.nu".content = ''
     mkdir ($nu.data-dir | path join "vendor/autoload")
     $env.STARSHIP_CONFIG = "${pkgs.starship.configuration.env.STARSHIP_CONFIG.data}"
-    ${pkgs.starship.wrap { shell = null; }}/bin/starship init nu | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")
+    ${pkgs.starship.wrap { shell = null; addFlag = [ "init" "nu" ]; }}/bin/starship | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")
   '';
 }
