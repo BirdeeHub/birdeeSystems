bf663b04e81aaf8a9fcd33e502f90ad747ae3d16
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2025-12-05 02:56:59 -0800

    Merged the frobnicate branch into master with a sprinkle of cheese
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/wrappers/nushell/default.nix

--- common/wrappers/nushell/default.nix
+++ common/wrappers/nushell/default.nix
@@ -1,5 +1,10 @@
 {config, pkgs, wlib, lib, ... }: {
   imports = [ wlib.wrapperModules.nushell ];
+  # # TODO: why this no work
+  # "config.nu".content = ''
+  #   source ${pkgs.starship.wrap { shell = "nu"; }}/bin/starship
+  # '';
+  # # when this does work, and is logically equivalent minus the extra source
   "config.nu".content = ''
     mkdir ($nu.data-dir | path join "vendor/autoload")
     ${pkgs.starship.wrap {
