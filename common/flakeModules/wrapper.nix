0a9962255711f8ac3345eca6717ddd96be8a09d2
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-23 02:49:55 -0800

    Added a secret feature that makes the app dance
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/flakeModules/wrapper.nix

--- common/flakeModules/wrapper.nix
+++ common/flakeModules/wrapper.nix
@@ -19,107 +19,103 @@ in
   imports = [
     inputs.flake-parts.flakeModules.modules
   ];
-  options = {
-    flake = mkOption {
-      type = types.submoduleWith {
-        modules = [
-          (
-            { config, ... }:
-            {
-              _file = file;
-              key = file;
-              options.wrapperModules = mkOption {
-                type = types.lazyAttrsOf types.deferredModule;
-                default = { };
-                description = ''
-                  contains unevaluated wrapper modules like from this library
+  options.flake = mkOption {
+    type = types.submoduleWith {
+      modules = [
+        (
+          { config, ... }:
+          {
+            _file = file;
+            key = file;
+            options.wrapperModules = mkOption {
+              type = types.lazyAttrsOf types.deferredModule;
+              default = { };
+              description = ''
+                contains unevaluated wrapper modules like from this library
 
-                  https://github.com/BirdeeHub/nix-wrapper-modules
-                '';
-              };
-              options.wrappedModules = mkOption {
-                type = types.lazyAttrsOf (wlib.types.subWrapperModuleWith { });
-                description = ''
-                  contains partially evaluated wrapperModules
-                '';
-              };
-              config.wrappedModules = config.wrapperModules;
-            }
-          )
+                https://github.com/BirdeeHub/nix-wrapper-modules
+              '';
+            };
+            options.wrappedModules = mkOption {
+              type = types.lazyAttrsOf (wlib.types.subWrapperModuleWith { });
+              description = ''
+                contains partially evaluated wrapperModules
+              '';
+            };
+            config.wrappedModules = config.wrapperModules;
+          }
+        )
+      ];
+    };
+  };
+  options.perSystem =
+    let
+      wrapped = config.flake.wrappedModules;
+    in
+    flake-parts-lib.mkPerSystemOption (
+      {
+        system,
+        pkgs,
+        config,
+        ...
+      }:
+      {
+        _file = file;
+        key = file;
+        options.wrapperPkgs = mkOption {
+          type = types.pkgs;
+          default = pkgs;
+        };
+        config.packages = builtins.mapAttrs (_: v: v.wrap { pkgs = config.wrapperPkgs; }) wrapped;
+      }
+    );
+  config.flake.modules.generic = config.flake.wrapperModules;
+  config.flake.modules.homeManager = builtins.mapAttrs (
+    n: v:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      options.wrapperModules.${n} = lib.mkOption {
+        default = { };
+        type = wlib.types.subWrapperModule [
+          v
+          {
+            config.pkgs = pkgs;
+            options.enable = lib.mkEnableOption n;
+          }
         ];
       };
-    };
-    perSystem =
-      let
-        wrapped = config.flake.wrappedModules;
-      in
-      flake-parts-lib.mkPerSystemOption (
-        {
-          system,
-          pkgs,
-          config,
-          ...
-        }:
-        {
-          _file = file;
-          key = file;
-          options.wrapperPkgs = mkOption {
-            type = types.pkgs;
-            default = pkgs;
-          };
-          config.packages = builtins.mapAttrs (_: v: v.wrap { pkgs = config.wrapperPkgs; }) wrapped;
-        }
-      );
-  };
-  config.flake.modules = {
-    generic = config.flake.wrapperModules;
-    homeManager = builtins.mapAttrs (
-      n: v:
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        options.wrapperModules.${n} = lib.mkOption {
-          default = { };
-          type = wlib.types.subWrapperModule [
-            v
-            {
-              config.pkgs = pkgs;
-              options.enable = lib.mkEnableOption n;
-            }
-          ];
-        };
-        config.home.packages = lib.mkIf config.wrapperModules.${n}.enable [
-          config.wrapperModules.${n}.wrapper
+      config.home.packages = lib.mkIf config.wrapperModules.${n}.enable [
+        config.wrapperModules.${n}.wrapper
+      ];
+    }
+  ) config.flake.wrapperModules;
+  config.flake.modules.nixos = builtins.mapAttrs (
+    n: v:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      options.wrapperModules.${n} = lib.mkOption {
+        default = { };
+        type = wlib.types.subWrapperModule [
+          v
+          {
+            config.pkgs = pkgs;
+            options.enable = lib.mkEnableOption n;
+          }
         ];
-      }
-    ) config.flake.wrapperModules;
-    nixos = builtins.mapAttrs (
-      n: v:
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      {
-        options.wrapperModules.${n} = lib.mkOption {
-          default = { };
-          type = wlib.types.subWrapperModule [
-            v
-            {
-              config.pkgs = pkgs;
-              options.enable = lib.mkEnableOption n;
-            }
-          ];
-        };
-        config.environment.systemPackages = lib.mkIf config.wrapperModules.${n}.enable [
-          config.wrapperModules.${n}.wrapper
-        ];
-      }
-    ) config.flake.wrapperModules;
-  };
+      };
+      config.environment.systemPackages = lib.mkIf config.wrapperModules.${n}.enable [
+        config.wrapperModules.${n}.wrapper
+      ];
+    }
+  ) config.flake.wrapperModules;
 }
