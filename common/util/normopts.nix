6fdf14618d16b125a5fb2940599dfde58d3ce53a
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-28 12:51:52 -0800

    Reversed the order of cheese and crackers on the plate. It's a sandwich now.
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/util/normopts.nix

--- common/util/normopts.nix
+++ common/util/normopts.nix
@@ -3,9 +3,11 @@
   wlib,
   collectOptions,
 }:
-pkgs:
-options:
-collectOptions {
-  inherit options;
-  transform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
-}
+pkgs: options:
+let
+  og_options = collectOptions {
+    inherit options;
+    transform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
+  };
+in
+og_options
