e92e31681a789d24f96595233178791a16161af6
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-28 12:52:58 -0800

    Fixed the flux capacitor again
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/util/normopts.nix

--- common/util/normopts.nix
+++ common/util/normopts.nix
@@ -3,8 +3,9 @@
   wlib,
   collectOptions,
 }:
-pkgs: options:
+{ graph, options, config, ... }:
 let
+  inherit (config) pkgs;
   og_options = collectOptions {
     inherit options;
     transform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
