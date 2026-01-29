3eea9fc259c2bf4785a771e2e3f9b821ffdda0dc
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-29 00:57:06 -0800

    Bounced a bug off the moon and caught it on the fender
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/util/normopts.nix
    	new file:   common/util/notes.nix
    	modified:   flake.lock
    	modified:   flake.nix

--- common/util/normopts.nix
+++ common/util/normopts.nix
@@ -3,12 +3,48 @@
   wlib,
   collectOptions,
 }:
-{ graph, options, config, ... }:
+{
+  graph,
+  options,
+  config,
+  ...
+}:
 let
   inherit (config) pkgs;
-  og_options = collectOptions {
-    inherit options;
-    transform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
-  };
+  zipper = builtins.zipAttrsWith (file: xs: {
+    inherit file;
+    description = builtins.foldl' (acc: v: acc // {
+      ${if v.desc.pre or "" != "" then "pre" else null} = v.desc.pre;
+      ${if v.desc.post or "" != "" then "post" else null} = v.desc.post;
+    }) {} xs;
+    maintainers = builtins.filter (v: v != null) (map (v: v.ppl or null) xs);
+  });
+  descriptions = map (v: {
+    ${v.file} = {
+      desc = v;
+    };
+  }) config.meta.description;
+  maintainers = map (v: {
+    ${v.file} = {
+      ppl = v;
+    };
+  }) config.meta.maintainers;
+  zipped = zipper (descriptions ++ maintainers);
+
+  # og_options = collectOptions {
+  #   inherit options;
+  #   transform = x: if builtins.elem "_module" x.loc then [ ] else [ x ];
+  # };
+  # renderVal =
+  #   v:
+  #   if v ? _type && v ? text then
+  #     if v._type == "literalExpression" then "`${toString v.text}`" else toString v.text
+  #   else if lib.isStringLike v then
+  #     "${toString v}"
+  #   else
+  #     v;
+  # illiterate = map (v: builtins.mapAttrs (n: renderVal) v) og_options;
+  # visible = lib.pipe illiterate [
+  # ];
 in
-og_options
+{ graph = graph; meta = zipped; }
---
+++ common/util/notes.nix
@@ -0,0 +1,163 @@
+{
+  graph = [
+    {
+      disabled = false;
+      file = "<unknown-file>";
+      imports = [ ];
+      key = ":anon-1";
+    }
+    {
+      disabled = false;
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/parts.nix";
+      imports = [
+        {
+          disabled = false;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/lib/core.nix";
+          imports = [ ];
+          key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/lib/core.nix";
+        }
+      ];
+      key = ":anon-2";
+    }
+    {
+      disabled = false;
+      file = "<unknown-file>";
+      imports = [ ];
+      key = ":anon-3";
+    }
+    {
+      disabled = false;
+      file = "/nix/store/ghjya315dqmcig1gq0c67h46d9av3p0v-source/common/wrappers";
+      imports = [
+        {
+          disabled = false;
+          file = "/nix/store/ghjya315dqmcig1gq0c67h46d9av3p0v-source/common/wrappers/git";
+          imports = [
+            {
+              disabled = false;
+              file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/wrapperModules/g/git/module.nix";
+              imports = [
+                {
+                  disabled = false;
+                  file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/default/module.nix";
+                  imports = [
+                    {
+                      disabled = false;
+                      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/symlinkScript/module.nix";
+                      imports = [ ];
+                      key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/symlinkScript/module.nix";
+                    }
+                    {
+                      disabled = false;
+                      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix";
+                      imports = [
+                        {
+                          disabled = false;
+                          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix";
+                          imports = [ ];
+                          key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix:anon-1";
+                        }
+                      ];
+                      key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix";
+                    }
+                  ];
+                  key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/default/module.nix";
+                }
+              ];
+              key = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/wrapperModules/g/git/module.nix";
+            }
+          ];
+          key = "/nix/store/ghjya315dqmcig1gq0c67h46d9av3p0v-source/common/wrappers/git";
+        }
+      ];
+      key = ":anon-4";
+    }
+    {
+      disabled = false;
+      file = "REPLFILE";
+      imports = [ ];
+      key = ":anon-5";
+    }
+  ];
+  meta = {
+    "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/lib/core.nix" = {
+      description = { };
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/lib/core.nix";
+      maintainers = [
+        {
+          email = null;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/lib/core.nix";
+          github = "BirdeeHub";
+          githubId = 85372418;
+          matrix = null;
+          name = "birdee";
+        }
+      ];
+    };
+    "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/default/module.nix" = {
+      description = {
+        pre = "This module imports both `wlib.modules.makeWrapper` and `wlib.modules.symlinkScript` for convenience\n\n## `wlib.modules.makeWrapper`\n\nAn implementation of the `makeWrapper` interface via type safe module options.\n\nAllows you to choose one of several underlying implementations of the `makeWrapper` interface.\n\nWherever the type includes `DAG` you can mentally substitute this with `attrsOf`\n\nWherever the type includes `DAL` or `DAG list` you can mentally substitute this with `listOf`\n\nHowever they also take items of the form `{ data, name ? null, before ? [], after ? [] }`\n\nThis allows you to specify that values are added to the wrapper before or after another value.\n\nThe sorting occurs across ALL the options, thus you can target items in any `DAG` or `DAL` within this module from any other `DAG` or `DAL` option within this module.\n\nThe `DAG`/`DAL` entries in this module also accept an extra field, `esc-fn ? null`\n\nIf defined, it will be used instead of the value of `options.escapingFunction` to escape that value.\n\n## `wlib.modules.symlinkScript`\n\nAdds extra options compared to the default `builderFunction` option value.\n\n---\n";
+      };
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/default/module.nix";
+      maintainers = [
+        {
+          email = null;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/default/module.nix";
+          github = "BirdeeHub";
+          githubId = 85372418;
+          matrix = null;
+          name = "birdee";
+        }
+      ];
+    };
+    "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix" = {
+      description = {
+        post = "---\n\n## The `makeWrapper` library:\n\nShould you ever need to redefine `config.wrapperFunction`, or use these options somewhere else,\nthis module doubles as a library for doing so!\n\n`makeWrapper = import wlib.modules.makeWrapper;`\n\nIf you import it like shown, you gain access to some values.\n\nFirst, you may modify the module itself.\n\nFor this it offers:\n\n`exclude_wrapper = true;` to stop it from setting `config.wrapperFunction`\n\n`wrapperFunction = ...;` to override the default `config.wrapperFunction` that it sets instead of excluding it.\n\n`exclude_meta = true;` to stop it from setting any values in `config.meta`\n\n`excluded_options = { ... };` where you may include `optionname = true`\nin order to not define that option.\n\nIn order to change these values, you change them in the set before importing the module like so:\n\n```nix\n  imports = [ (import wlib.modules.makeWrapper // { excluded_options.wrapperVariants = true; }) ];\n```\n\nIt also offers 4 functions for using those options to generate build instructions for a wrapper\n\n- `wrapAll`: generates build instructions for the main target and all variants\n- `wrapMain`: generates build instructions for the main target\n- `wrapVariants`: generates build instructions for all variants but not the main target\n- `wrapVariant`: generates build instructions for a single variant\n\nAll 4 of them return a string that can be added to the derivation definition to build the specified wrappers.\n\nThe first 3, `wrapAll`, `wrapMain`, and `wrapVariants`, are used like this:\n\n(import wlib.modules.makeWrapper).wrapAll {\n  inherit config wlib;\n  inherit (pkgs) callPackage; # or `inherit pkgs`;\n};\n\nThe 4th, `wrapVariant`, has an extra `name` argument:\n\n(import wlib.modules.makeWrapper).wrapVariant {\n  inherit config wlib;\n  inherit (pkgs) callPackage; # or `inherit pkgs`;\n  name = \\\"attribute\\\";\n};\n\nWhere `attribute` is an attribute of the `config.wrapperVariants` set\n\nOther than whatever options from the `wlib.modules.makeWrapper` module\nare defined in the `config` variable passed,\neach one relies on `config` containing `binName`, `package`, and `exePath`.\n\nIf `config.exePath` is not a string or is an empty string,\n`config.package` will be the full path wrapped.\nOtherwise, it will wrap `\"\${config.package}/\${config.binName}`.\n\nIf `config.binName` or `config.package` are not provided it will return an empty string for that target.\n\nIn addition, if a variant has `enable` set to `false`, it will also not be included in the returned string.\n";
+        pre = "An implementation of the `makeWrapper` interface via type safe module options.\n\nAllows you to choose one of several underlying implementations of the `makeWrapper` interface.\n\nImported by `wlib.modules.default`\n\nWherever the type includes `DAG` you can mentally substitute this with `attrsOf`\n\nWherever the type includes `DAL` or `DAG list` you can mentally substitute this with `listOf`\n\nHowever they also take items of the form `{ data, name ? null, before ? [], after ? [] }`\n\nThis allows you to specify that values are added to the wrapper before or after another value.\n\nThe sorting occurs across ALL the options, thus you can target items in any `DAG` or `DAL` within this module from any other `DAG` or `DAL` option within this module.\n\nThe `DAG`/`DAL` entries in this module also accept an extra field, `esc-fn ? null`\n\nIf defined, it will be used instead of the value of `options.escapingFunction` to escape that value.\n\nIt also has a set of submodule options under `config.wrapperVariants` which allow you\nto duplicate the effects to other binaries from the package, or add extra ones.\n\nEach one contains an `enable` option, and a `mirror` option.\n\nThey also contain the same options the top level module does, however if `mirror` is `true`,\nas it is by default, then they will inherit the defaults from the top level as well.\n\nThey also have their own `package`, `exePath`, and `binName` options, with sensible defaults.\n\n---\n";
+      };
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix";
+      maintainers = [
+        {
+          email = null;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/makeWrapper/module.nix";
+          github = "BirdeeHub";
+          githubId = 85372418;
+          matrix = null;
+          name = "birdee";
+        }
+      ];
+    };
+    "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/symlinkScript/module.nix" = {
+      description = {
+        pre = "Adds extra options compared to the default `builderFunction` option value.\n\nImported by `wlib.modules.default`\n\n---\n";
+      };
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/symlinkScript/module.nix";
+      maintainers = [
+        {
+          email = null;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/modules/symlinkScript/module.nix";
+          github = "BirdeeHub";
+          githubId = 85372418;
+          matrix = null;
+          name = "birdee";
+        }
+      ];
+    };
+    "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/wrapperModules/g/git/module.nix" = {
+      description = {
+        pre = "Nix uses git for all sorts of things. Including fetching flakes!\n\nSo if you put this one in an overlay, name it something other than `pkgs.git`!\n\nOtherwise you will probably get infinite recursion.\n\nThe vast majority of other packages do not have this issue. And,\ndue to the passthrough of `.override` and `.overrideAttrs`,\nmost other packages are safe to replace with their wrapped counterpart in overlays directly.\n";
+      };
+      file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/wrapperModules/g/git/module.nix";
+      maintainers = [
+        {
+          email = null;
+          file = "/nix/store/fwq3ggyxrfgn3xis0zm545kd3vy8j18j-source/wrapperModules/g/git/module.nix";
+          github = "BirdeeHub";
+          githubId = 85372418;
+          matrix = null;
+          name = "birdee";
+        }
+      ];
+    };
+  };
+}
