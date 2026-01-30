10ad2f3af140442ffff1bb9a58e2e393a7d9dcb0
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-29 18:04:46 -0800

    Added an extra layer of indirection to the algorithm for no discernible reason
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/util/docs/rendermd.nix

--- common/util/docs/rendermd.nix
+++ common/util/docs/rendermd.nix
@@ -11,6 +11,8 @@ let
       if v._type == "literalExpression" then "`${toString v.text}`" else toString v.text
     else if lib.isStringLike v && !builtins.isString v then
       builtins.unsafeDiscardStringContext "<${v.name or v}>"
+    else if builtins.isString v then
+      builtins.unsafeDiscardStringContext v
     else if builtins.isList v then
       map sanitize v
     else if builtins.isAttrs v then
