a5d7a542444727b749a7dc1339fbb98804d6d45c
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-29 17:56:20 -0800

    Optimized the banana sorting algorithm for maximum peel efficiency
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/util/docs/rendermd.nix

--- common/util/docs/rendermd.nix
+++ common/util/docs/rendermd.nix
@@ -10,7 +10,7 @@ let
     if v ? _type && v ? text then
       if v._type == "literalExpression" then "`${toString v.text}`" else toString v.text
     else if lib.isStringLike v && !builtins.isString v then
-      builtins.unsafeDiscardStringContext (v.name or "${v}")
+      builtins.unsafeDiscardStringContext "<${v.name or v}>"
     else
       v;
   normed = normWrapperDocs { inherit options graph; };
