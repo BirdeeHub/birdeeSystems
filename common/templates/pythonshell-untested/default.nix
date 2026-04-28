3290fb55ad6f2528d6b54fd2678aa591d2853c8b
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-04-27 23:37:52 -0700

    Added a feature to make coffee taste like old shoes
    (auto-msg qwen2.5-coder:7b)
    On branch installmods
    Your branch is up to date with 'origin/installmods'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/templates/pythonshell-untested/default.nix
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   common/templates/pythonshell-untested/flake.nix
    	modified:   util/mkLuaStuff.nix

--- common/templates/pythonshell-untested/default.nix
+++ common/templates/pythonshell-untested/default.nix
@@ -1,6 +1,6 @@
-  # TODO: TRY THIS OUT
-  #NOTE: I HAVE NO CLUE IF THIS WORKS BUT IM DOING SOMETHING ELSE RIGHT NOW
-  # JUST SAVING THIS FOR LATER TO TRY IT. GOT THIS FROM A REDDIT COMMENT BUT DIDNT BOOKMARK SO IDK WHERE...
+# TODO: TRY THIS OUT
+# NOTE: I HAVE NO CLUE IF THIS WORKS BUT IM DOING SOMETHING ELSE RIGHT NOW
+# JUST SAVING THIS FOR LATER TO TRY IT. GOT THIS FROM A REDDIT COMMENT BUT DIDNT BOOKMARK SO IDK WHERE...
 { pkgs, inputs, lib, writeText, makeWrapper, writeShellScript, stdenv, ... }: let
   procPath = with pkgs; [
     coreutils
@@ -10,28 +10,29 @@
     gnugrep
     gawk
   ];
-  luaEnv = pkgs.lua5_2.withPackages (lpkgs: with lpkgs; [
-    luafilesystem
-    cjson
-    busted
-    inspect
-    http
-    cqueues
-    stdlib
-  ]);
+  luaEnv = pkgs.lua5_2.withPackages (
+    lpkgs: with lpkgs; [
+      luafilesystem
+      cjson
+      busted
+      inspect
+      http
+      cqueues
+      stdlib
+    ]
+  );
   appname = "REPLACE_ME";
-in
-stdenv.mkDerivation (let
+in stdenv.mkDerivation (let
   launcher = writeShellScript "${appname}" ''
     ${luaEnv}/bin/lua ${./src/${appname}.lua} "$@"
   '';
 in {
   name = "${appname}";
   src = ./.;
-  buildInputs = with pkgs; [  ];
+  buildInputs = with pkgs; [];
   propagatedBuildInputs = with pkgs; [ luaEnv ] ++ procPath;
   nativeBuildInputs = with pkgs; [ makeWrapper ];
-  propagatedNativeBuildInputs = with pkgs; [  ];
+  propagatedNativeBuildInputs = with pkgs; [];
   buildPhase = ''
     source $stdenv/setup
     mkdir -p $out/bin
