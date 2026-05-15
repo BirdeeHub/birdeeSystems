0bbb23dd3c2fcb84eb5adf30a79110610a7db6d0
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-05-15 06:21:55 -0700

    Added a new feature that allows users to customize their cheese preferences[11D[K preferences in the app
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/modules/lua/embed/default.nix

--- common/modules/lua/embed/default.nix
+++ common/modules/lua/embed/default.nix
@@ -1,12 +1,14 @@
 { toLuaModule, lua, ... }: toLuaModule (lua.stdenv.mkDerivation (finalAttrs: let
   luafinal = finalAttrs.passthru.luaModule or lua;
+  inherit (builtins) head split foldl';
+  pipe = foldl' (v: f: f v);
 in {
   pname = "embed";
   version = "dev";
   src = ./embed.c;
   dontUnpack = true;
   env.LUA_INC = "${luafinal}/include";
-  env.LUA_CPATH_DIR = dirOf (builtins.head luafinal.pkgs.luaLib.luaCPathList);
+  env.LUA_CPATH_DIR = pipe luafinal.LuaPathSearchPaths [ head (split "[?]") head dirOf ];
   buildPhase = ''
     runHook preBuild;
     mkdir -p $out/$LUA_CPATH_DIR
