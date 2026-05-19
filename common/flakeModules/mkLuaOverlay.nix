ed52e59df705b964e8598b6c87414992ded96d27
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-05-19 03:44:33 -0700

    Updated the README to include a section on how to debug your rice cooker
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/flakeModules/mkLuaOverlay.nix

--- common/flakeModules/mkLuaOverlay.nix
+++ common/flakeModules/mkLuaOverlay.nix
@@ -10,17 +10,17 @@
 }:
   assert builtins.isList versions || throw "lua versions must be a list of strings containing `lua.luaAttr` names corresponding to `pkgs.luaInterpreters`!";
   assert controlType == "build" || controlType == "exclude" || throw ''controlType must be "build" or "exclude"'';
-final: prev: let
-  final-versions = if controlType == "build" then
-    prev.lib.intersectLists versions (builtins.attrNames prev.luaInterpreters)
-  else
-    prev.lib.pipe prev.luaInterpreters [
-      builtins.attrNames
-      (builtins.filter (x: !builtins.elem x versions))
-    ];
-in {
+final: prev: {
   # https://github.com/NixOS/nixpkgs/blob/dd950ec2fda73b76273d3812a1a5cf35c77b4b69/pkgs/top-level/all-packages.nix#L4866-L4907
-  luaInterpreters = prev.luaInterpreters // prev.lib.pipe final-versions [
+  luaInterpreters = prev.luaInterpreters // prev.lib.pipe (
+    if controlType == "build" then
+      prev.lib.intersectLists versions (builtins.attrNames prev.luaInterpreters)
+    else
+      prev.lib.pipe prev.luaInterpreters [
+        builtins.attrNames
+        (builtins.filter (x: !builtins.elem x versions))
+      ]
+  ) [
     (map (v: prev.lib.nameValuePair v packageOverrides))
     builtins.listToAttrs
     (builtins.mapAttrs (
