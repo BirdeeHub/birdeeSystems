bdbf45bde4d023a1b2ce8983ee54c1860ebe41d7
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-06-29 16:08:12 -0700

    yep

--- common/features/opencode/module.nix
+++ common/features/opencode/module.nix
@@ -17,17 +17,14 @@
             npm = "@ai-sdk/openai-compatible";
             name = "Ollama (local)";
             options = {
-              baseURL = "http://localhost:11434/v1";
+              baseURL = "http://10.0.0.101:11434/v1";
             };
             models = {
-              "gpt-oss:20b" = {
-                name = "gpt-oss:20b";
+              "qwen3.6:35b-a3b" = {
+                name = "qwen3.6:35b-a3b";
               };
-              "qwen3:14b" = {
-                name = "qwen3:14b";
-              };
-              "qwen3:8b" = {
-                name = "qwen3:8b";
+              "qwen3.5:122b" = {
+                name = "qwen3.5:122b";
               };
             };
           };
