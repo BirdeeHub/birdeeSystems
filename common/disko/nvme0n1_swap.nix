40dcfca3c42b5f7ec2735e76024fe6a26ddb4093
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-07-08 10:36:28 -0700

    disk

--- common/disko/nvme0n1_swap.nix
+++ common/disko/nvme0n1_swap.nix
@@ -39,6 +39,23 @@
           };
         };
       };
+      homeDisk = {
+        device = "/dev/nvme1n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            home = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/home";
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
