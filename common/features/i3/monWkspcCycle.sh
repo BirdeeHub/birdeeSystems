e987418bd0a4d109df0f4632f2d791ae94bdd443
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-08-29 12:02:24 -0700

    Added an easter egg for the compiler
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   monWkspcCycle.sh

--- common/features/i3/monWkspcCycle.sh
+++ common/features/i3/monWkspcCycle.sh
@@ -3,8 +3,8 @@ ipccmd=i3-msg
 [[ -n "$1" ]] && ipccmd=swaymsg
 #when you run it, it moves the current workspace to the next monitor in the list.
 #Add a keybind to this script in ~/.i3/config so that you can do it with a keypress
-currentWkspc="$($ipccmd -t get_workspaces | jq -r '.[] | select(.focused==true).num')"
-currentMon="$($ipccmd -t get_outputs | jq -r ".[] | select(.current_workspace == \"$currentWkspc\") | .name")"
+currentWkspc="$("$ipccmd" -t get_workspaces | jq -r '.[] | select(.focused==true).num')"
+currentMon="$("$ipccmd" -t get_outputs | jq -r ".[] | select(.current_workspace == \"$currentWkspc\") | .name")"
 if [[ -n "$1" ]]; then
     readarray -t activeMons <<< "$(swaymsg -t get_outputs -r | jq -r '.[] | select(.active) | .name')"
 else
@@ -14,7 +14,7 @@ fi
 activeMons+=( "${activeMons[0]}" )
 found="false"
 for mon in "${activeMons[@]}"; do
-    [[ "$found" == "true" && "$mon" != "$currentMon" ]] && exec $ipccmd "workspace number $currentWkspc, move workspace to output $mon"
-    [[ "$mon" == "$currentMon" ]] && found="true" && bash -c "$ipccmd \"workspace number $currentWkspc\"" 
+    [[ "$found" == "true" && "$mon" != "$currentMon" ]] && exec "$ipccmd" "workspace number $currentWkspc, move workspace to output $mon"
+    [[ "$mon" == "$currentMon" ]] && found="true" && "$ipccmd" "workspace number $currentWkspc"
     #selecting current workspace will swap focus to the previous workspace you focused temporarily, because you cannot move focused windows. If there was only 1 monitor, on the next iteration, the exec will not trigger, and we will switch it back.
 done
