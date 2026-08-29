d7c0dd53c9d4f700dbcb67e21a39009d2ab3149c
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-08-29 06:51:05 -0700

    Added an easter egg for the compiler
    (auto-msg qwen2.5-coder:7b)
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/features/quickshell/module.nix

--- common/features/quickshell/module.nix
+++ common/features/quickshell/module.nix
@@ -18,53 +18,66 @@
         NotifyDeez {}
       }
     '';
+    options.colors = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {};
+    };
+    options.notifyConfig = lib.mkOption {
+      type = wlib.types.attrsRecursive;
+      default = {};
+    };
+    options.barConfig = lib.mkOption {
+      type = wlib.types.attrsRecursive;
+      default = {};
+    };
+    config.notifyConfig = {
+      fontSize = 14;
+      timeout = 5000;
+    };
+    config.colors = {
+      darkest = "#1a1b26";
+      dark = "#292F34";
+      medium = "#3e4452";
+      light = "#047180";
+      bright = "#80a0ff";
+      white = "white";
+      black = "black";
+      good = "#9ECE6A";
+      warn = "#F2D674";
+      alert = "#BA02F2";
+    };
+    config.barConfig = {
+      fontSize = 11;
+      height = 15;
+      battery = {
+          width = 50;
+          heightPercent = 90;
+          warnPercent = 60;
+          critPercent = 30;
+      };
+      workspaces = {
+          width = 20;
+          spacing = 4;
+          heightPercent = 90;
+      };
+      tray = {
+          iconPercentOfHeight = 90;
+          spacing = 8;
+          rightMargin = 4;
+      };
+      stats = {
+          isSway = false;
+          i3status = lib.getExe config.i3status.wrapper;
+          leftPadding = 8;
+          rightPadding = 8;
+      };
+    };
     config.constructFiles.configJS = {
       relPath = dirOf config.constructFiles.generatedConfig.relPath + "/config.js";
-      content = /*js*/ ''
-        const notify = {
-            fontSize: 14,
-            timeout: 5000
-        }
-
-        const colors = {
-            darkest: "#1a1b26",
-            dark: "#292F34",
-            medium: "#3e4452",
-            light: "#047180",
-            bright: "#80a0ff",
-            white: "white",
-            black: "black",
-            good: "#9ECE6A",
-            warn: "#F2D674",
-            alert: "#BA02F2"
-        }
-
-        const bar = {
-            fontSize: 11,
-            height: 15,
-            battery: {
-                width: 50,
-                heightPercent: 90,
-                warnPercent: 60,
-                critPercent: 30,
-            },
-            workspaces: {
-                width: 20,
-                spacing: 4,
-                heightPercent: 90,
-            },
-            tray: {
-                iconPercentOfHeight: 90,
-                spacing: 8,
-                rightMargin: 4
-            },
-            stats: {
-                isSway: false,
-                i3status: ${builtins.toJSON (lib.getExe config.i3status.wrapper)},
-                leftPadding: 8,
-                rightPadding: 8
-            }
-        }
+      content = ''
+        const notify = ${builtins.toJSON config.notifyConfig}
+        const colors = ${builtins.toJSON config.colors}
+        const bar = ${builtins.toJSON config.barConfig}
       '';
     };
     config.buildCommand.mkCfg = ''
